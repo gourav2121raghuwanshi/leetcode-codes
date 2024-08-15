7684e73534b1778ebe3d581e9507e4d9736cb974
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-08-15 10:47:28 +0530

    Time: 4 ms (46.49%), Space: 14.8 MB (21.27%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,38 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/*
+To enhance efficiency, we will involve two pointers, a fast pointer and a slow
+pointer. The fast-moving pointer will initially be exactly N nodes ahead of the
+slow-moving pointer. After which, both of them will move one step at a time.
+When the fast pointer reaches the last node, i.e., the L-th node, the slow is
+guaranteed to be at the (L-N)-th node, where L is the total length of the linked
+list.
+
+
+*/
 class Solution {
-    int len(ListNode*head){
-        int c=0;
-        while(head){
-            ++c;
-            head=head->next;
-        }
-        return c;
-    }
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         if(!head || !head->next) return NULL;
-        int l=len(head);
-        int fromStart=l-n;
-        // cout<<fromStart<<endl;
-        if(fromStart==0) return head->next;
-        ListNode*curr=head;
-        ListNode*prev;
-        while(curr && fromStart--){
-            prev=curr;
-            curr=curr->next;
-        }
-        if(curr && prev)
-        prev->next=curr->next;
-        return head;
+        ListNode*fast=head;
+        ListNode*slow=head;
+        ListNode*prev=head;
+        for(int i=1;i<=n;i++) fast=fast->next;
 
+        while(fast){
+            prev=slow;
+            fast=fast->next;
+            slow=slow->next;
+        }
+        if(slow==head) return head->next;
+        prev->next=slow->next;
+
+
+        return head;
     }
 };
