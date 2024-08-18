41a20bb6bb60cf7ba63022832ffd41b05a981bc0
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-08-18 13:54:29 +0530

    Time: 135 ms (79%), Space: 126.6 MB (69.92%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,25 +1,19 @@
 class Solution {
-    void make(vector<int>& a, vector<int>& b, vector<int>& ans) {
-        int i = 0;
-        int j = 0;
-        while (i < a.size() && j < b.size()) {
-            ans.push_back(a[i++]);
-            ans.push_back(b[j++]);
-        }
-    }
-
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-
-        vector<int> a, b;
-        for (auto& i : nums) {
-            if (i < 0)
-                a.push_back(i);
-            else
-                b.push_back(i);
+        int m=nums.size();
+        vector<int>ans(m);
+        int p=0;
+        int n=1;
+        for(auto&i:nums){
+            if(i<0){
+                ans[n]=i;
+                n+=2;
+            }else{
+                ans[p]=i;
+                p+=2;
+            }
         }
-        vector<int> ans;
-        make(b, a, ans);
         return ans;
     }
 };
