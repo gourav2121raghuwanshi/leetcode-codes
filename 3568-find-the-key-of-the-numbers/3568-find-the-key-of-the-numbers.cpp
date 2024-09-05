bd380db3b5094be93e3f12a95c88f2841c2bbecd
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-09-05 07:31:55 +0530

    Time: 0 ms (100%), Space: 8 MB (99.68%) - LeetHub

--- 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.cpp
+++ 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int generateKey(int num1, int num2, int num3) {
-        string a=to_string(num1);
-        string b=to_string(num2);
-        string c=to_string(num3);
-        int n=min(a.size(),min(b.size(),c.size()));
-        int m=max(a.size(),max(b.size(),c.size()));
-        string ans;
-        int i=a.size()-1;
-        int j=b.size()-1;
-        int k=c.size()-1;
-        for(int d=0;d<n;d++){
-            char ch=min(a[i--],min(b[j--],c[k--]));
-            ans.push_back(ch);
+        int ans = 0;
+        int mul = 1;
+        while (num1 && num2 && num3) {
+            int r1 = num1 % 10;
+            int r2 = num2 % 10;
+            int r3 = num3 % 10;
+            ans = min(r1, min(r2, r3)) * mul + ans;
+            mul *= 10;
+            num1 /= 10;
+            num2 /= 10;
+            num3 /= 10;
         }
-        while(ans.size()<m){
-            ans.push_back('0');
-        }
-        reverse(ans.begin(),ans.end());
-        return stoi(ans);
+        return ans;
     }
 };
