8d2621993cc8984ece63680a4d0a9d3e3276dcc8
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-11-28 15:33:44 +0530

    Time: 11 ms (9.19%), Space: 34.3 MB (9.28%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
-        if (k == 1) {
-            return nums;
-        }
-        set<int> mark;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] != nums[i - 1] + 1)
-                mark.insert(i);
-        }
-        int n = nums.size();
-
-        vector<int> ans;
-        for (int i = 0; i <= n - k; i++) {
-            int nrs = i;
-            if (mark.upper_bound(nrs) != mark.end()) {
-                auto it = mark.upper_bound(nrs);
-                if (*it > k + i - 1) {
-                    ans.push_back(nums[i + k - 1]);
-                } else {
-                    ans.push_back(-1);
-                }
-            } else {
-                ans.push_back(nums[i + k - 1]);
+       vector<int>ans;
+       int n=nums.size();
+       for(int i=0;i<=n-k;i++) {
+        bool f=true;
+        for(int j=i;j<i+k;j++){
+            if(j==i) continue;
+            else if(nums[j]<=nums[j-1] || nums[j-1]+1!=nums[j]){
+                f=false;
+                break;
             }
         }
-
-        return ans;
+        if(f) ans.push_back(nums[i+k-1]);
+        else ans.push_back(-1);
+       }
+       return ans;
     }
 };
