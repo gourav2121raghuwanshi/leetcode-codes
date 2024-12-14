cbbe153ccfe191a61c4e127b081f96064e1f8539
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-14 21:51:39 +0530

    Time: 0 ms (100%), Space: 21.8 MB (17.05%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -6,18 +6,15 @@ public:
         for(int i=0;i<n;++i){
             if(intervals[i][1]<newInterval[0]){
                 ans.push_back(intervals[i]);
-            }else if(intervals[i][0]>newInterval[1]){
+            }else if(newInterval[1]<intervals[i][0]){
                 ans.push_back(newInterval);
                 newInterval=intervals[i];
-            }
-            else{
-                newInterval[0]=min(intervals[i][0],newInterval[0]);
-                newInterval[1]=max(intervals[i][1],newInterval[1]);
-                
+            }else{
+                newInterval[0]=min(newInterval[0],intervals[i][0]);
+                newInterval[1]=max(newInterval[1],intervals[i][1]);
             }
         }
         ans.push_back(newInterval);
-
         return ans;
     }
 };
