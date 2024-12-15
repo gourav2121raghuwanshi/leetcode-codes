0640d3e82f0c439553f62325fb469554e76dea5c
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-15 12:01:40 +0530

    Time: 2064 ms (10.3%), Space: 7.7 MB (27.42%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    int solve(int n){
+        if(n<=1) return 1;
+        int ans=0;
+        for(int i=1;i<=n;++i){
+            ans+= solve(i-1)*solve(n-i);
+        }
+        return ans;
+    }
+public:
+    int numTrees(int n) {
+        return solve(n);
+    }
+};
