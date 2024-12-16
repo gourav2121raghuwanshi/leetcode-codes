e85523847b32a3fdf4892a621b5fe128a1093c1c
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-16 21:50:16 +0530

    Time: 19 ms (74.45%), Space: 50.3 MB (39.93%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,35 +1,22 @@
-class Trie
-{
-    class node
-    {
+class Trie {
+    class node {
     public:
-        node *link[26];
-        bool end;
-        node()
-        {
-            end = false;
+        node* link[26];
+        bool end = false;
+        node() {
             for (int i = 0; i < 26; ++i)
-            {
-                link[i] = NULL;
-            }
+                link[i] = nullptr;
         }
     };
 
-    node *root;
-
 public:
-    Trie()
-    {
-        root = new node();
-    }
+    Trie() {}
+    node* root = new node();
 
-    void insert(string word)
-    {
-        node *curr = root;
-        for (auto &i : word)
-        {
-            if (curr->link[i - 'a'] == nullptr)
-            {
+    void insert(string word) {
+        node* curr = root;
+        for (auto& i : word) {
+            if (curr->link[i - 'a'] == nullptr) {
                 curr->link[i - 'a'] = new node();
             }
             curr = curr->link[i - 'a'];
@@ -37,13 +24,10 @@ public:
         curr->end = true;
     }
 
-    bool search(string word)
-    {
-        node *curr = root;
-        for (auto &i : word)
-        {
-            if (curr->link[i - 'a'] == nullptr)
-            {
+    bool search(string word) {
+        node* curr = root;
+        for (auto& i : word) {
+            if (curr->link[i - 'a'] == nullptr) {
                 return false;
             }
             curr = curr->link[i - 'a'];
@@ -51,13 +35,10 @@ public:
         return curr->end == true;
     }
 
-    bool startsWith(string prefix)
-    {
-        node *curr = root;
-        for (auto &i : prefix)
-        {
-            if (curr->link[i - 'a'] == nullptr)
-            {
+    bool startsWith(string word) {
+        node* curr = root;
+        for (auto& i : word) {
+            if (curr->link[i - 'a'] == nullptr) {
                 return false;
             }
             curr = curr->link[i - 'a'];
@@ -65,3 +46,11 @@ public:
         return true;
     }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
