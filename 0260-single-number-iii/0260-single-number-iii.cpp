069c019143150f04b5843bc934c508f128ff3847
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-17 11:44:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int xo = 0;
-        for (auto& i : nums)
-            xo ^= i;
-        long long c = 0;
-        // cout << xo;
-        while (xo) {
-            if (xo&1)
-                break;
+        int xo=0;
+        for(auto&i:nums) xo^=i;
+        int c=0;
+        while(xo>0){
+            if(xo&1) break;
             ++c;
-            xo = xo >> 1;
+            xo/=2;
         }
-        // cout<<c;
-        int a = 0, b = 0;
-        for (const auto& i : nums) {
-            if (i & (1 << c))
-                a ^= i;
-            else
-                b ^= i;
+        int a=0;
+        int b=0;
+        for(auto&i:nums){
+            if(i&(1<<c)) a|=i;
+            else b|=i;
         }
-
-        return {a, b};
+        return {a,b};
     }
 };
