a9d9dc8d7ab19b9af1edef282c8a95d8719851b8
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-17 11:44:37 +0530

    Time: 0 ms (100%), Space: 13.7 MB (52.13%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -4,7 +4,7 @@ public:
         int xo=0;
         for(auto&i:nums) xo^=i;
         int c=0;
-        while(xo>0){
+        while(xo){
             if(xo&1) break;
             ++c;
             xo/=2;
@@ -12,8 +12,8 @@ public:
         int a=0;
         int b=0;
         for(auto&i:nums){
-            if(i&(1<<c)) a|=i;
-            else b|=i;
+            if(i&(1<<c)) a^=i;
+            else b^=i;
         }
         return {a,b};
     }
