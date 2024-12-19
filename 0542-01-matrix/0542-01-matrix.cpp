da91a03e1462f543e33819eea0dca146c0961824
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-19 18:02:45 +0530

    Time: 19 ms (63.91%), Space: 34.7 MB (70.77%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        queue<pair<int,int>>q;
-        vector<vector<int>>ans(n,vector<int>(m,1e9));
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(mat[i][j]==0){
-                    q.push({i,j});
-                    ans[i][j]=0;
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> ans(n, vector<int>(m, -1));
+        queue<pair<int, int>> q;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (mat[i][j] == 0) {
+                    q.push({i, j});
+                    ans[i][j] = 0;
                 }
             }
         }
-        int cnt=1;
-        int row[]={0,0,1,-1};
-        int col[]={1,-1,0,0};
-        while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                auto front=q.front();
-                int r=front.first;
-                int c=front.second;
+        int cnt = 1;
+        vector<pair<int, int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                auto front = q.front();
                 q.pop();
-                for(int k=0;k<4;++k){
-                    int dr=r+row[k];
-                    int dc=c+col[k];
-                    if(dr>=0 && dc>=0 && dr<n && dc<m && mat[dr][dc]==1){
-                        ans[dr][dc]=min(ans[dr][dc],cnt);
-                        q.push({dr,dc});
-                        mat[dr][dc]=0;
+                for (auto& j : dir) {
+                    int nr = front.first + j.first;
+                    int nc = front.second + j.second;
+                    if (nr >= 0 && nc >= 0 && nr < n && nc < m &&
+                        ans[nr][nc] == -1) {
+                        ans[nr][nc] = cnt;
+                        q.push({nr, nc});
                     }
                 }
             }
