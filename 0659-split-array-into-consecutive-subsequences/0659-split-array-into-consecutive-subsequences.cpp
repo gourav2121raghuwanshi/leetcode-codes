37501b8f836cca8e3658bd0cf06c488d267ba7fd
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-19 21:42:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        unordered_map<int,int>end;
+
+        for(int i=0;i<nums.size();++i){
+            ++freq[nums[i]];
+        }
+        for(auto&i:nums){
+            if(freq[i]==0) continue;
+            --freq[i];
+            if(end[i-1]>0){
+                --end[i-1];
+                end[i]=1;
+            }else if(freq[i+1]>0 && freq[i+2]>0){
+                --freq[i+1];
+                --freq[i+2];
+                end[i+2]++;
+            }else return false;
+        }
+        return true;
+    }
+};
