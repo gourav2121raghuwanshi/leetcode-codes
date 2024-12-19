f6f2f670d4bb359aafc12409ccda2fd512765529
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-19 22:41:07 +0530

    Time: 0 ms (100%), Space: 9.1 MB (12.56%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,32 +1,22 @@
 class Solution {
     int dp[101][101];
-public:
-    bool solve(int idx, int open, string& s) {
-        if (open < 0) return false;
-        if (idx == s.size()) return (open == 0);
-        if (dp[idx][open] != -1) return dp[idx][open];
-        
-        bool ans = false;
-        if (s[idx] == '*') {
-            ans = ans || solve(idx + 1, open + 1, s);
-            if(ans) return true;
-            ans = ans || solve(idx + 1, open - 1, s);
-              if(ans) return true;
-            ans = ans || solve(idx + 1, open, s);
-              if(ans) return true;
-          
-        } 
-        else if (s[idx] == '(') {
-            ans = ans || solve(idx + 1, open + 1, s);
-              if(ans) return true;
-        } else {
-            ans = ans || solve(idx + 1, open - 1, s);
-              if(ans) return true;
+    bool solve(int i,string &s,int open){
+        if(i==s.size()){
+            return open==0;
         }
-        return dp[idx][open] = ans;
+        if(open<0) return false;
+        if(dp[i][open]!=-1) return dp[i][open];
+        if(s[i]=='('){
+            return dp[i][open] = solve(i+1,s,open+1);
+        }else if(s[i]==')'){
+            return dp[i][open] = solve(i+1,s,open-1);
+        }else {
+            return dp[i][open] = solve(i+1,s,open+1) || solve(i+1,s,open-1) || solve(i+1,s,open);
+        }
     }
+public:
     bool checkValidString(string s) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, s);
+        memset(dp,-1,sizeof(dp));
+        return solve(0,s,0);
     }
 };
