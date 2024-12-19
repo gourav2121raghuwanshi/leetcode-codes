525664501e694a4a4c3e304b934639e1b4d36368
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-19 15:58:36 +0530

    Time: 3 ms (96.51%), Space: 12.4 MB (16.18%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,21 +1,25 @@
 class Solution {
-    int ans=0;
-    void solve(int j,int val,int n){
-        if(j==n+1) ++ans;
-        if(j>n+1) return;
-
+    
+    int dp[16][(1<<16)];
+    int solve(int j,int val,int n){
+        if(j==n+1) return 1;
+        if(j>n+1) return 0;
+        if(dp[j][val]!=-1) return dp[j][val];
+        int v=0;
         for(int i=1;i<=n;++i){
             if(val&(1<<i) && (i%j==0 || j%i==0)){
                 val^=(1<<i);
-                solve(j+1,val,n);
+                v+=solve(j+1,val,n);
                 val^=(1<<i);
             }
         }
+        return dp[j][val]=v;
+
     }
 public:
     int countArrangement(int n) {
         int val=(1<<(n+1))-1;
-        solve(1,val,n);
-        return ans;
+        memset(dp,-1,sizeof(dp));
+        return solve(1,val,n);
     }
 };
