3912b3e16ea0a9d90804ba40ed347d7edc60c345
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-20 09:47:00 +0530

    Time: 0 ms (100%), Space: 21.4 MB (35.65%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& as) {
         vector<int> ans;
-        int n = as.size();
-        for (int i = 0; i < n; i++) {
 
-            while (as[i] < 0 && !ans.empty() && ans.back() > 0 &&
-                   ans.back() < abs(as[i]))
-                ans.pop_back();
-            if (as[i] < 0 && !ans.empty() && ans.back() > 0 &&
-                ans.back() > abs(as[i]))
-                continue;
-            if (as[i] < 0 && !ans.empty() && ans.back() > 0 &&
-                ans.back() == abs(as[i])) {
-                ans.pop_back();
-            } else
+        for (int i = 0; i < as.size(); ++i) {
+            // Case 1: No collision, push the asteroid
+            if (ans.empty() || as[i] > 0 || ans.back() < 0) {
                 ans.push_back(as[i]);
+            } else {
+                // Case 2: Potential collision
+                while (!ans.empty() && ans.back() > 0 && ans.back() < abs(as[i])) {
+                    ans.pop_back(); // Remove smaller positive asteroid
+                }
+
+                // Case 3: Same size, both explode
+                if (!ans.empty() && ans.back() > 0 && ans.back() == abs(as[i])) {
+                    ans.pop_back();
+                }
+                // Case 4: Push negative asteroid if no more collision
+                else if (ans.empty() || ans.back() < 0) {
+                    ans.push_back(as[i]);
+                }
+            }
         }
 
         return ans;
