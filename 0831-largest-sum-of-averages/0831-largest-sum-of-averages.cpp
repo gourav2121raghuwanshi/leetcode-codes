8c90292efa15647562d6d9a4e63939f2abbfbc09
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-20 20:06:12 +0530

    Time: 3 ms (93.09%), Space: 9.9 MB (74.94%) - LeetHub

--- 0831-largest-sum-of-averages/0831-largest-sum-of-averages.cpp
+++ 0831-largest-sum-of-averages/0831-largest-sum-of-averages.cpp
@@ -1,24 +1,21 @@
 class Solution {
     double dp[101][101];
-    double solve(int i, vector<int>& nums, int k) {
-        if (i >= nums.size() || k < 0)
-            return 0;
-        if(k==0) return INT_MIN;
-        if (dp[i][k] != 0)
-            return dp[i][k];
-        double ans = 0;
-        double sum = 0;
-        for (int curr = i; curr < nums.size(); curr++) {
-            double len = curr - i + 1;
-            sum += nums[curr];
-            // if (k - 1 >= 0)
-                ans = max(ans, sum / len + solve(curr + 1, nums, k - 1));
+    double solve(int i,vector<int>& nums, int k){
+        if(k<0 || i>=nums.size()) return 0;
+        if(k==0) return -1e9;
+        double sum=0;
+        double ans=0;
+        if(dp[i][k]!=0) return dp[i][k];
+        for(int idx=i;idx<nums.size();++idx){
+            sum+=nums[idx];
+            int len=idx-i+1;
+            ans=max(ans,sum/len+solve(idx+1,nums,k-1));
         }
         return dp[i][k] = ans;
     }
 public:
     double largestSumOfAverages(vector<int>& nums, int k) {
-        memset(dp, 0, sizeof(dp));
-        return solve(0, nums, k);
+        memset(dp,0,sizeof(dp));
+        return solve(0,nums,k);
     }
 };
