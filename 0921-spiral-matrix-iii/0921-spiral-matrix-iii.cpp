6d69e3c8040df35fdf92c79776b9ab476b5640d3
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-21 11:38:39 +0530

    Time: 12 ms (7.53%), Space: 15.7 MB (18.82%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart,
-                                        int cStart) {
-        // east or west : increase by 1.
-        vector<vector<int>> ans;
-        int c = 0;
-        int i = rStart;
-        int j = cStart;
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>>ans;
+        vector<pair<int,int>>direction={{0,1},{1,0},{0,-1},{-1,0}};
+        int ele=0;
+        int i=rStart;
+        int j=cStart;
+        int dir=0;
+        while(ans.size()<rows*cols){
+            if(dir==0 || dir==2) ++ele;
 
-        int dir = 0;
-        // directions to follow :  e  , s, w , n
-        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int number_of_elements_to_take = 0;
-        while (ans.size() < rows * cols) {
-
-            if (dir == 0 || dir == 2)
-                ++number_of_elements_to_take;
-                
-            for (int col = 0; col < number_of_elements_to_take; ++col) {
-                if (i >= 0 && j >= 0 && i < rows && j < cols) {
-                    ans.push_back({i, j});
+            for(int col=0;col<ele;++col){
+                if(i>=0 && j>=0 && i<rows && j<cols){
+                    ans.push_back({i,j});
                 }
-                i += directions[dir].first;
-                j += directions[dir].second;
+                i+=direction[dir].first;
+                j+=direction[dir].second;
             }
-
-            dir = (dir + 1) % 4;
+            dir=(dir+1)%4;
         }
         return ans;
     }
