3838227c3f1a50876ca8daddecef305204b02ba0
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-23 20:50:10 +0530

    Time: 0 ms (100%), Space: 74 MB (39.61%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int>ans(n+1);
-        // logic : passengers l pe aa jayenge , and r+1 pe utar jayenge
+        vector<int>ans(n+2,0);
         for(auto&i:bookings){
-            int l=i[0];
-            int r=i[1];
-            int val=i[2];
-            ans[l]+=val;
-            if(r+1<=n)
-            ans[r+1]-=val;
+            ans[i[0]]+=i[2];
+            ans[i[1]+1]-=i[2];
         }
-        for(int i=1;i<ans.size();++i){
-            ans[i]+=ans[i-1];
-            ans[i-1]=ans[i];
+        vector<int>v;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=ans[i];
+            v.push_back(sum);
         }
-        ans.pop_back();
-        return ans;
+        return v;
+        
     }
 };
