87f63d7c98ae62551c51205e5602a899d23a25ed
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-27 18:14:10 +0530

    Time: 228 ms (10.32%), Space: 19.5 MB (15.64%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        vector<int>v(5,0);
-        // a 0
-        // e 1
-        // i 2
-        // o 3
-        // u 4
-        map<vector<int>,int>mp;
-        mp[v]=-1;
-        unordered_set<char>st={'a','e','i','o','u'};
-        unordered_map<char,int>mapping;
-        mapping['a']=0;
-        mapping['e']=1;
-        mapping['i']=2;
-        mapping['o']=3;
-        mapping['u']=4;
+       vector<int>freq(5,0) ;
+       unordered_map<char,int>mapping;
+       mapping['a']=0;
+       mapping['e']=1;
+       mapping['i']=2;
+       mapping['o']=3;
+       mapping['u']=4;
+
+       unordered_set<char>st={
+        'a','e','i','o','u'
+       };
+       map<vector<int>,int>mp;
+       mp[freq]=-1;
+
         int ans=0;
-        for(int j=0;j<s.size();++j){
-            char curr=s[j];
-            if(st.count(curr)){
-                ++v[mapping[curr]];
-                v[mapping[curr]]%=2;
+
+        for(int i=0;i<s.size();++i){
+            if(st.count(s[i])){
+            ++freq[mapping[s[i]]];
+            freq[mapping[s[i]]]%=2;
             }
-            if(mp.count(v)){
-                ans=max(ans,j-mp[v]);
+            if(mp.count(freq)){
+                ans=max(ans,i-mp[freq]);
             }else{
-                mp[v]=j;
+                mp[freq]=i;
             }
         }
-        return ans;
+
+        return ans; 
     }
 };
