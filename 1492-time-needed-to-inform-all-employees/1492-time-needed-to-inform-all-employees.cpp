41e81526a8a753289c3d9aa2ab3d9d8f1b96f6f5
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-27 20:59:05 +0530

    Time: 249 ms (17.81%), Space: 166.6 MB (9.06%) - LeetHub

--- 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        unordered_map<int,vector<int>>adj;
-        int root=-1;
-        for(int i=0;i<n;++i){
-            if(manager[i]==-1){
-                root=i;
-            }else{
+        unordered_map<int, vector<int>> adj;
+
+        // Build adjacency list
+        for (int i = 0; i < n; ++i) {
+            if (manager[i] != -1) {
                 adj[manager[i]].push_back(i);
             }
         }
-        int ans=0;
-        queue<pair<int,int>>q;
-        q.push({root,0});
-        while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                auto front=q.front(); q.pop();
-                int node=front.first;
-                int cost=front.second;
-                ans=max(ans,cost);
-                for(auto&j:adj[node]){
-                    q.push({j,cost+informTime[node]});
-                }
+
+        // BFS traversal
+        int ans = 0;
+        queue<pair<int, int>> q; // {node, cumulative time}
+        q.push({headID, 0});
+
+        while (!q.empty()) {
+            auto [node, cost] = q.front();
+            q.pop();
+            ans = max(ans, cost);
+
+            for (int subordinate : adj[node]) {
+                q.push({subordinate, cost + informTime[node]});
             }
         }
-        return ans;
 
+        return ans;
     }
 };
