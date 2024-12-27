a14a0b86617b6d45c62b4bae652ed6638f3de8be
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-27 21:57:09 +0530

    Time: 19 ms (83.35%), Space: 12.4 MB (59.13%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
-        int ans = 0;
-        int n=rating.size();
-        for (int i = 0; i < n; ++i) {
-            int ls = 0, rs = 0;
-            int ll = 0, rl = 0;
-            for (int j = 0; j < i; ++j) {
-                if (rating[j] < rating[i])
-                    ++ls;
-                else if (rating[j] > rating[i])
-                    ++ll;
+        int ans=0;
+
+        for(int i=0;i<rating.size();++i){
+            int ls=0,ll=0;
+            int rs=0,rl=0;
+            for(int j=0;j<i;++j){
+                if(rating[j]<rating[i]) ++ls;
+                else if(rating[j]>rating[i]) ++ll;
             }
-            for (int j = i + 1; j < n; ++j) {
-                if (rating[j] < rating[i])
-                    ++rs;
-                else if (rating[j] > rating[i])
-                    ++rl;
+            for(int j=i+1;j<rating.size();++j){
+                if(rating[j]<rating[i]) ++rs;
+                else if(rating[j]>rating[i]) ++rl;
             }
-            ans += ls * rl + ll * rs;
+            ans+=(ls*rl)+(ll*rs);
         }
+
         return ans;
     }
 };
