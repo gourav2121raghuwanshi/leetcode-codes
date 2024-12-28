bb52928ccb29a87beb7885e896206b3922e69c80
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-28 22:50:08 +0530

    Time: 13 ms (52.78%), Space: 182 MB (44.79%) - LeetHub

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/1568-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/1568-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -11,24 +11,27 @@
  */
 class Solution {
     int ans=0;
-    void solve(TreeNode*root,vector<int>freq){
-        if(!root){
+    void solve(TreeNode*root,vector<int>&freq){
+        if(!root)
+            return ;
+        ++freq[root->val];
+        if(!root->left && !root->right){
             int cnt=0;
             for(auto&i:freq){
                 if(i&1) ++cnt;
             }
             if(cnt<=1) ++ans;
-            return;
         }
-        ++freq[root->val];
+        
         solve(root->left,freq);
         solve(root->right,freq);
+        --freq[root->val];
     }
 public:
     int pseudoPalindromicPaths (TreeNode* root) {
         if(!root)return 0;
         vector<int>freq(10,0);
         solve(root,freq);
-        return ans/2;
+        return ans;
     }
 };
