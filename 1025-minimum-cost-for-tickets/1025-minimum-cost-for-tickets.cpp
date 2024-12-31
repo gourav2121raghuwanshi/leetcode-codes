e201b4353c631bb01880e29bff5dab7db7f881a4
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-12-31 16:42:07 +0530

    Time: 0 ms (100%), Space: 13.4 MB (23.42%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -3,26 +3,27 @@ class Solution {
     int solve(int i, vector<int>& days, vector<int>& costs) {
         if (i >= days.size())
             return 0;
-        if (dp[i] != -1)
-            return dp[i];
-        int day0 = costs[0] + solve(i + 1, days, costs);
+        if(dp[i]!=-1) return dp[i];
+        int day1 = costs[0] + solve(i + 1, days, costs);
+        int day7 = costs[1];
         int j = i;
-        while (j < days.size() && days[j] < days[i] + 7) {
+        int cango = days[i] + 7;
+        while (j < days.size() && days[j] < cango)
             ++j;
-        }
-        int day7 = costs[1] + solve(j, days, costs);
-        
-        while (j < days.size() && days[j] < days[i] + 30) {
-            ++j;
-        }
-        int day30 = costs[2] + solve(j, days, costs);
+        day7 += solve(j, days, costs);
 
-        return dp[i] = min({day0, day7, day30});
+        int day30 = costs[2];
+        j = i;
+        cango = days[i] + 30;
+        while (j < days.size() && days[j] < cango)
+            ++j;
+        day30 += solve(j, days, costs);
+        return dp[i] = min({day1, day7, day30});
     }
 
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        memset(dp, -1, sizeof(dp));
+        memset(dp,-1,sizeof(dp));
         return solve(0, days, costs);
     }
 };
