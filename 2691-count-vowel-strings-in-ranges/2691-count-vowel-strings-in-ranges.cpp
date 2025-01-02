c4083bbb1e3309f352ce9926742433d05a53c020
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-01-02 09:38:37 +0530

    Time: 0 ms (100%), Space: 70 MB (59.07%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
-        // unordered_set<int>valid;
-        unordered_map<int,int>valid;
-
-        unordered_set<char>vowel={
-            'a','e','i','o','u'
-        };
-        int c=0;
-        for(int j=0;j<words.size();++j){
-            string i=words[j];
-            if(vowel.count(i[0]) && vowel.count(i[i.size()-1])){
-                ++c;
-               valid[j]=c;
-            }else valid[j]=c;
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+        int n = words.size();
+        
+        // Precompute cumulative counts in a vector
+        vector<int> valid(n + 1, 0); // Cumulative count array with an extra 0 at the start
+        for (int i = 0; i < n; ++i) {
+            valid[i + 1] = valid[i] + (vowels.count(words[i][0]) && vowels.count(words[i].back()));
         }
-        vector<int>ans;
-        for(auto&i:queries){
-            int start=i[0];
-            int end=i[1];
-            cout<<valid[start]<<" , "<<valid[end]<<endl;
-            if(!valid.count(start-1))
-            ans.push_back(valid[end]);
-            else
-            ans.push_back(valid[end]-valid[start-1]);
+        
+        // Process queries
+        vector<int> ans;
+        for (auto& query : queries) {
+            int start = query[0];
+            int end = query[1];
+            ans.push_back(valid[end + 1] - valid[start]);
         }
         return ans;
     }
