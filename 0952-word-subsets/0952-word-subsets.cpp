22772bf4c8f3b8cf6a2e176a9e7b1f3d4776b468
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-01-10 09:36:57 +0530

    Time: 56 ms (59.82%), Space: 106.9 MB (33.79%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.cpp
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-    vector<string>ans;
-    vector<vector<int>>freq;
-    vector<int>maxReq(26,0);
-    for(auto&i:words2){
-        vector<int>v(26,0);
-        for(auto&j:i){
-            ++v[j-'a'];
+        vector<string> ans;
+        vector<int> maxReq(26, 0);
+        for (auto& i : words2) {
+            vector<int> v(26, 0);
+            for (auto& j : i) {
+                ++v[j - 'a'];
+            }
+            for (int j = 0; j < 26; ++j) {
+                maxReq[j] = max(maxReq[j], v[j]);
+            }
         }
-        for(int j=0;j<26;j++){
-            maxReq[j]=max(maxReq[j],v[j]);
-        }
-    }
-    
-    for(int kk=0;kk<words1.size();++kk){
-        string i=words1[kk];
-        bool f=true;
-        vector<int>v(26,0);
-        for(auto&j:i){
-            ++v[j-'a'];
-        }
-    
-        
-            for(int j=0;j<26;j++){
-                if(maxReq[j]>v[j]){
-                    f=false;
+        int n = words1.size();
+        for (int kk = 0; kk < n; ++kk) {
+            string i = words1[kk];
+            bool f = true;
+            vector<int> v(26, 0);
+            for (auto& j : i) {
+                ++v[j - 'a'];
+            }
+
+            for (int j = 0; j < 26; ++j) {
+                if (maxReq[j] > v[j]) {
+                    f = false;
                     break;
                 }
-            
+            }
+
+            if (f)
+                ans.push_back(i);
         }
-         
-        if(f) ans.push_back(i);
-    }
-    return ans;
+        return ans;
     }
 };
