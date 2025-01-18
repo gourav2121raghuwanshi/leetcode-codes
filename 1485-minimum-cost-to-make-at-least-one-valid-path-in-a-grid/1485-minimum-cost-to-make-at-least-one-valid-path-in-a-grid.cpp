169d397d689431ffdd878f24350d9d5b744b97bd
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-01-18 08:44:42 +0530

    Time: 21 ms (65.4%), Space: 18.2 MB (90.01%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -7,39 +7,36 @@ public:
         pq.push({0, {0, 0}});
         int n = grid.size();
         int m = grid[0].size();
-        vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        vector<vector<int>> price(n, vector<int>(m, 1e9));
-        
-        price[0][0] = 0; // Set starting cell cost to 0
-        
-        while (!pq.empty()) {
-            auto front = pq.top();
-            pq.pop();
-            int x = front.second.first;
-            int y = front.second.second;
-            int cost = front.first;
+        vector<pair<int, int>> dir = { {0, 1}, {0, -1}, {1, 0}, {-1, 0}
+    };
+    vector<vector<int>>price(n,vector<int>(m,1e9));
+    price[0][0]=0;
+    while (!pq.empty()) {
+        auto front = pq.top();
+        pq.pop();
+        int x = front.second.first;
+        int y = front.second.second;
+        int cost = front.first;
+        if (x == n - 1 && y == m - 1)
+            return cost;
+        for (int i = 0; i <= 3; i++) {
+            if (grid[x][y] == i + 1) {
 
-            // If the target cell is reached, return the cost
-            if (x == n - 1 && y == m - 1)
-                return cost;
-
-            // Explore all four directions
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dir[i].first;
-                int ny = y + dir[i].second;
-
-                // Check if the next cell is within bounds
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    int newCost = cost + (grid[x][y] == i + 1 ? 0 : 1);
-                    
-                    // Update cost if a better path is found
-                    if (newCost < price[nx][ny]) {
-                        price[nx][ny] = newCost;
-                        pq.push({newCost, {nx, ny}});
-                    }
+                if (dir[i].first + x >= 0 && dir[i].second + y >= 0 &&
+                    dir[i].first + x < n && dir[i].second + y < m && price[dir[i].first + x ][ dir[i].second + y]>cost) {
+                    pq.push({cost, {dir[i].first + x, dir[i].second + y}});
+                    price[dir[i].first + x ][ dir[i].second + y]=cost;
+                }
+            }else{
+                  if (dir[i].first + x >= 0 && dir[i].second + y >= 0 &&
+                    dir[i].first + x < n && dir[i].second + y < m &&  price[dir[i].first + x ][ dir[i].second + y]>cost+1) {
+                    pq.push({cost+1, {dir[i].first + x, dir[i].second + y}});
+                    price[dir[i].first + x ][ dir[i].second + y]=cost+1;
                 }
             }
         }
-        return -1; // If the target cell is not reachable
     }
-};
+    return -1;
+}
+}
+;
