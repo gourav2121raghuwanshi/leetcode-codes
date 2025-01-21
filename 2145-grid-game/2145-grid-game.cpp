74b1e956e4009498a3ff2e58bc9551340ab36c92
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-01-21 10:29:25 +0530

    Time: 7 ms (30.77%), Space: 66.6 MB (76.92%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,23 +1,24 @@
 class Solution {
+    // maximizing first robot will not work , try minimizing the second robot directly 
 public:
     long long gridGame(vector<vector<int>>& grid) {
-        long long row_one_sum = 0;
+        long long row_one_total_sum = 0;
         int m = grid[0].size();
-        for (int j = 0; j < m; j++) {
-            row_one_sum += grid[0][j];
+        for (int j = 0; j < m; ++j) {
+            row_one_total_sum += grid[0][j];
         }
 
         long long given = LLONG_MAX;
         long long row_two_sum = 0;
-        long long s=0;
-        for (int i = 0; i < m; i++) {
-            s += grid[0][i];
-
-            long long first_line = row_one_sum - s;
+        long long row_one_curr_sum=0;
+        for (int i = 0; i < m; ++i) {
+            row_one_curr_sum += grid[0][i];
+            long long first_line = row_one_total_sum - row_one_curr_sum;
             long long second_line = row_two_sum;
 
-            if (given > max(first_line, second_line)) {
-                given = max(first_line, second_line);
+            long long curr_given=max(first_line, second_line);
+            if (given > curr_given) {
+                given = curr_given;
             }
             row_two_sum += grid[1][i];
         }
