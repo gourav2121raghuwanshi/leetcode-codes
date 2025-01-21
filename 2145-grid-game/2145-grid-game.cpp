03801fc103c8c67388a94a667b72f40144594be4
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-01-21 10:27:13 +0530

    Time: 4 ms (47.51%), Space: 66.7 MB (76.92%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
-        long long total_sum = 0;
+        long long row_one_sum = 0;
         int m = grid[0].size();
-        vector<long long> pref1(m);
-        vector<long long> pref2(m);
-        for (int i = 0; i < 2; i++) {
-            long long s = 0;
-            for (int j = 0; j < m; j++) {
-                s += grid[i][j];
-                total_sum += grid[i][j];
-                if (i == 0)
-                    pref1[j] = s;
-                else
-                    pref2[j] = s;
-            }
+        for (int j = 0; j < m; j++) {
+            row_one_sum += grid[0][j];
         }
-        // long long taken_out = 0;
-        int switchi = 0;
-        long long given=LLONG_MAX;
-        
+
+        long long given = LLONG_MAX;
+        long long row_two_sum = 0;
+        long long s=0;
         for (int i = 0; i < m; i++) {
-            long long first_line =pref1[m-1]-(pref1[i]);
-            long long second_line=(i-1>=0?pref2[i-1]:0);
-            
-            if (given > max(first_line , second_line)) {
-                given = max(first_line , second_line);
-                switchi=i;
+            s += grid[0][i];
+
+            long long first_line = row_one_sum - s;
+            long long second_line = row_two_sum;
+
+            if (given > max(first_line, second_line)) {
+                given = max(first_line, second_line);
             }
+            row_two_sum += grid[1][i];
         }
 
         return given;
