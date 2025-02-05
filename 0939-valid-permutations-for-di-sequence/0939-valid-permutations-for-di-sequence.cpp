ea1b522124d409653533c11bbe9aa97f37e23ec7
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-02-05 22:53:41 +0530

    Time: 76 ms (11.9%), Space: 8.3 MB (70.63%) - LeetHub

--- 0939-valid-permutations-for-di-sequence/0939-valid-permutations-for-di-sequence.cpp
+++ 0939-valid-permutations-for-di-sequence/0939-valid-permutations-for-di-sequence.cpp
@@ -1,37 +1,14 @@
 class Solution {
     int num;
     const int mod = 1e9 + 7;
-    // unordered_map<string, int> mp;
-
-    // string getKey(int i, int prev, vector<bool>& vis) {
-    //     string s;
-    //     s.push_back(i + '0');
-    //     s.push_back(':');
-    //     s.push_back(prev + '0');
-    //     s.push_back(':');
-    //     for (auto i : vis) {
-    //         if (i == false) {
-    //             s.push_back('0');
-    //             s.push_back(':');
-    //         } else {
-    //             s.push_back('1');
-    //             s.push_back(':');
-    //         }
-    //     }
-    //     return s;
-    // }
-    // unordered_map<string, int> dp;
     int dp[202][202];
     int solve(int i, int prev, string& s, vector<bool>& vis) {
         if (i >= s.size())
             return 1;
         if(dp[i][prev]!=-1) return dp[i][prev];
-        // string key = getKey(i, prev, vis);
-        // if (dp.count(key))
-        //     return dp[key];
         int ans = 0;
         if (s[i] == 'I') {
-            for (int j = prev + 1; j <= num; j++) {
+            for (int j = prev + 1; j <= num; ++j) {
                 if (!vis[j]) {
                     vis[j] = 1;
                     ans = (ans + solve(i + 1, j, s, vis)) % mod;
@@ -39,7 +16,7 @@ class Solution {
                 }
             }
         } else {
-            for (int j = 0; j < prev; j++) {
+            for (int j = 0; j < prev; ++j) {
                 if (!vis[j]) {
                     vis[j] = 1;
                     ans = (ans + solve(i + 1, j, s, vis)) % mod;
@@ -47,8 +24,8 @@ class Solution {
                 }
             }
         }
-
-        return dp[i][prev] = ans;
+        if(prev!=-1) return dp[i][prev]=ans;
+        return  ans;
     }
 
 public:
