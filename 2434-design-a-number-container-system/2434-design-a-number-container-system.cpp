a7fa11be410284ac4fa93807947d423ab7433a89
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-02-08 10:42:36 +0530

    Time: 126 ms (39.38%), Space: 196 MB (45.63%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -1,28 +1,25 @@
 class NumberContainers {
 public:
-    unordered_map<int, int> idx_number;
-    unordered_map<int, set<int>> number_idx;
+    unordered_map<int, int> idx_number;  // Maps index -> number
+    unordered_map<int, set<int>> number_idx;  // Maps number -> set of indices
+    
     NumberContainers() {}
 
     void change(int index, int number) {
-        if (idx_number.count(index)) {
-            int itsNum = idx_number[index];
-            number_idx[itsNum].erase(index);
+        auto it = idx_number.find(index);
+        if (it != idx_number.end()) {  // If index already exists, remove it from previous number's set
+            int oldNumber = it->second;
+            number_idx[oldNumber].erase(index);
+            if (number_idx[oldNumber].empty()) {
+                number_idx.erase(oldNumber);
+            }
         }
-        idx_number[index] = number;
-        number_idx[number].insert(index);
+        idx_number[index] = number;  // Update the mapping
+        number_idx[number].emplace(index);
     }
 
     int find(int number) {
-        if (number_idx.count(number) && !number_idx[number].empty())
-            return *number_idx[number].begin();
-        return -1;
+        auto it = number_idx.find(number);
+        return (it != number_idx.end() && !it->second.empty()) ? *it->second.begin() : -1;
     }
 };
-
-/**
- * Your NumberContainers object will be instantiated and called as such:
- * NumberContainers* obj = new NumberContainers();
- * obj->change(index,number);
- * int param_2 = obj->find(number);
- */
