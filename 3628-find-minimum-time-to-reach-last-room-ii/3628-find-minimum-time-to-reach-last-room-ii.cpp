8db6f07dd809cb341137892544b2209f5d6dba9f
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-02-13 18:35:06 +0530

    Time: 801 ms (27.5%), Space: 167.5 MB (26.71%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int n = moveTime.size();
-        int m = moveTime[0].size();
+        //     matrix    // nXm
+        //               // val = (i,j) = i*m+j
+        //               // row  = val/m
+        //               // col  = val%m
 
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>, greater<>>
-            pq;
-        //         // nXm
-        //         //val = (i,j) = i*m+j
-        //         // row  = val/m
-        //         // col  = val%m
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        dist[0][0] = 0;
-        pq.push({0, {1, 0}}); // {cost, {alternating_move, position}}
 
-        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int n=moveTime.size();
+        int m=moveTime[0].size();
+        priority_queue<vector<int>,vector<vector<int>>,greater<>>pq;
+        pq.push({0,1,0}); // cost,c,val(row,col)
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        dist[0][0]=0;
 
-        while (!pq.empty()) {
-            auto i = pq.top();
+        vector<pair<int,int>>dir={
+            {0,1},{0,-1},{-1,0},{1,0}
+        };
+        while(!pq.empty()){
+            auto front=pq.top();
             pq.pop();
-            int cost = i.first;
-            int c = i.second.first;
-            int val = i.second.second;
-            int row = val / m;
-            int col = val % m;
-            if (row == n - 1 && col == m - 1) {
-                return cost;
-            }
-            for (auto& j : directions) {
-                int nr = row + j.first;
-                int nc = col + j.second;
-                if (nr >= 0 && nc >= 0 && nr < n && nc < m) {
-                    int newCost = max(cost + c, moveTime[nr][nc] + c);
-                    if (dist[nr][nc] > newCost) {
-                        int newVal = nr * m + nc;
-                        int nextC = (c == 1 ? 2 : 1);
-                        pq.push({newCost, {nextC, newVal}});
-                        dist[nr][nc] = newCost;
+            int cost=front[0];
+            int c=front[1];
+            int val=front[2];
+            int row=val/m;
+            int col=val%m;
+
+            if(row==n-1 && col==m-1) return cost;
+            for(auto&j:dir){
+                int r=row+j.first;
+                int cl=col+j.second;
+                if(r>=0 && cl>=0 && r<n && cl<m){
+                    int newCost = max(cost+c,moveTime[r][cl]+c);
+                    if(newCost<dist[r][cl]){
+                        dist[r][cl]=newCost;
+                        int newc=c==1?2:1;
+                        int nidx=r*m+cl;
+                        pq.push({newCost,newc,nidx});
                     }
                 }
             }
