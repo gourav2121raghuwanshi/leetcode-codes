8b473dcfe4e3316a45673ad37fe29a2a34adc17a
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-02-14 09:42:14 +0530

    Time: 15 ms (85.88%), Space: 78.1 MB (56.86%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,22 +1,25 @@
 class ProductOfNumbers {
 public:
     vector<int> v;
-    
-    int prod=1;
-    ProductOfNumbers() {v.push_back(1);}
+
+    ProductOfNumbers() { v.push_back(1); }
 
     void add(int num) {
-        if(num==0){
+
+        if (num != 0) {
+            if (!v.empty())
+                v.push_back(v.back() * num);
+            else
+                v.push_back(num);
+        } else {
             v.clear();
             v.push_back(1);
-        }else {
-            v.push_back(v.back()*num);
         }
     }
-
     int getProduct(int k) {
-       if(v.size()<=k) return 0;
-       return v.back()/v[v.size()-k-1];
+        if (v.size() <= k)
+            return 0;
+        return v.back() / v[v.size() - k -1 ];
     }
 };
 
