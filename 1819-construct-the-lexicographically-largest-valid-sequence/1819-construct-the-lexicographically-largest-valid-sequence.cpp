f85332b601a19ce4ebd495c92a2be87e7ae6e81a
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-02-16 12:06:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (64.94%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
@@ -1,38 +1,37 @@
 class Solution {
-    vector<int> ans;
-    bool solve(int idx, vector<int>& freq, vector<int>& temp, int n) {
+    
+    bool solve(int idx, vector<int>& freq, vector<int>& ans, int n) {
 
-        if (idx == temp.size()) { // we have Reached a answer
+        if (idx == ans.size()) { // we have Reached a answer
             // since we want lexographically Largest , hence return upon getting
             // First answer as we had started From Back to Front
-            ans = max(ans, temp);
             return 1;
         }
-        if (temp[idx] != -1) // already Occupied
-            return solve(idx + 1, freq, temp, n);
+        if (ans[idx] != -1) // already Occupied
+            return solve(idx + 1, freq, ans, n);
 
         for (int i = n; i >= 1; --i) {
             if (freq[i] > 0) {
                 --freq[i];     // Decrease the frequency
-                temp[idx] = i; // Fill the idx
+                ans[idx] = i; // Fill the idx
                 if (i == 1) {  // 1 has to be placed only Once
-                    bool a = solve(idx + 1, freq, temp, n);
+                    bool a = solve(idx + 1, freq, ans, n);
                     if (a)
                         return a;
-                } else if (i != 1 && idx + i < temp.size() &&
-                           temp[idx + i] ==
+                } else if (i != 1 && idx + i < ans.size() &&
+                           ans[idx + i] ==
                                -1) { // Others Twice at given Distance
-                    temp[idx + i] = i;
+                    ans[idx + i] = i;
                     --freq[i];
-                    bool a = solve(idx + 1, freq, temp, n);
+                    bool a = solve(idx + 1, freq, ans, n);
                     if (a)
                         return a;
                     // backTrack
                     ++freq[i];
-                    temp[idx + i] = -1;
+                    ans[idx + i] = -1;
                 }
                 // backTrack
-                temp[idx] = -1; // make Idx empty again
+                ans[idx] = -1; // make Idx empty again
                 ++freq[i];      // Increase elemets frequency
             }
         }
@@ -50,21 +49,21 @@ public:
             freq[i] = 2;
 
 
-        vector<int> temp(req, -1);
+        vector<int> ans(req, -1); 
 
         for (int i = n; i >= 1; i--) {
-            --freq[i];
-            if (i != 1) {
-                temp[i] = i;
+            --freq[i]; //take Once
+            if (i != 1) { // if not 1 , take twice
+                ans[i] = i;
                 --freq[i];
             }
-            temp[0] = i;
-            if (solve(1, freq, temp, n))
+            ans[0] = i;
+            if (solve(1, freq, ans, n))
                 return ans;
             ++freq[i];
-            temp[0]=-1;
+            ans[0]=-1;
              if (i != 1) {
-                temp[i] = -1;
+                ans[i] = -1;
                 ++freq[i];
             }
         }
