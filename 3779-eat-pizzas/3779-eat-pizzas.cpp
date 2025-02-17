fadfa490c3eb006e125a11eea19892d92504656c
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-02-17 19:16:22 +0530

    Time: 98 ms (11.11%), Space: 180 MB (33.33%) - LeetHub

--- 3779-eat-pizzas/3779-eat-pizzas.cpp
+++ 3779-eat-pizzas/3779-eat-pizzas.cpp
@@ -8,24 +8,24 @@ public:
         // 1: (1,2,2,5)
         // 2: (2,2,4,4)
         // 3: (3,3,3,5)
-        int n=pizzas.size();
-        int totalMoves=n/4;
-        int odd=(totalMoves+1)/2;
-        int even=totalMoves-odd;
-        long long ans=0;
-        int i=3*odd;
-        sort(pizzas.begin(),pizzas.end());
-        while(odd--){
-            ans+=1LL*pizzas.back();
-            pizzas.pop_back();
-        }
-        i=pizzas.size()-1;
-        while(even--){
+        
+        int n = pizzas.size();
+        int totalMoves = n / 4;
+        int odd = (totalMoves + 1) / 2;
+        int even = totalMoves - odd;
+        long long ans = 0;
+        int i = pizzas.size() - 1;
+        sort(pizzas.begin(), pizzas.end());
+
+        while (odd--) {
+             ans += 1LL * pizzas[i];
             --i;
-            ans+=1LL*pizzas[i];
+        }
+        while (even--) {
+            --i;
+            ans += 1LL * pizzas[i];
             --i;
         }
         return ans;
-
     }
 };
