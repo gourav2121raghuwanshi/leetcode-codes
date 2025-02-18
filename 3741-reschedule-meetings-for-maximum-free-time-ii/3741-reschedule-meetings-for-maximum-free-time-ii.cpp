a9a3de85872288b4a4458a2f3f69d04b0c037631
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-02-18 21:19:47 +0530

    Time: 17 ms (60.55%), Space: 153.7 MB (40.39%) - LeetHub

--- 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -27,32 +27,14 @@ public:
 
         for(int i=0;i<n;++i){
             int len=endTime[i]-startTime[i];
-            // cout<<len+gap[i]+gap[i+1]<<endl;
             if(pref[i]>=len || suff[i]>=len)
             ans=max(ans,len+gap[i]+gap[i+1]);
+        }
 
-        }
-        maxi=0;
-        for(int i=0;i<n;++i){
-            maxi=max(maxi,gap[i]);
-            if(maxi>=endTime[i]-startTime[i]){
-                ans=max(ans,endTime[i]-startTime[i]+gap[i+1]);
-            }
-            
-        }
         for(int i=0;i<gap.size()-1;i++){
-            // int len=endTime[i]-startTime[i];
-            // if(gap[i]>=len){
                 ans=max(ans,gap[i]+gap[i+1]);
-            // }
-            // if(gap[i+1]>=len){
-            //     ans=max(ans,gap[i+1]+gap[i]);
-            // }
         }
 
-        // for(auto&i:gap) cout<<i<<','; cout<<endl;
-        // for(auto&i:pref) cout<<i<<','; cout<<endl;
-        // for(auto&i:suff) cout<<i<<',';
         return ans;
     }
 };
