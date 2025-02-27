6f1b8d7a2f3d526d191d1fd9203e0034833745da
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-02-27 18:11:09 +0530

    Time: 366 ms (33.97%), Space: 149.9 MB (21.37%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -3,21 +3,34 @@ public:
     int lenLongestFibSubseq(vector<int>& arr) {
         int ans = 0;
         unordered_map<int, int> mp;
-        
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
         // Store index of each element
-        for (int i = 0; i < arr.size(); ++i) {
+        for (int i = 0; i < n; ++i) {
             mp[arr[i]] = i;
         }
-        int n=arr.size();
-        for (int i = 0; i < arr.size() && (n-i+1>ans); ++i) {
-            for (int j = i + 1; j < arr.size(); ++j) {
-                int req = arr[i] + arr[j]; 
+
+        for (int i = 0; i < n && (n - i + 1 > ans); ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (dp[i][j] != -1) continue; // Already computed
+
+                int req = arr[i] + arr[j];
                 int idx = j, idx1 = i;
                 int l = 2;  
 
-               while (mp.find(req) != mp.end() && mp[req] > idx) {
+                while (mp.find(req) != mp.end() && mp[req] > idx) {
+                    int k = mp[req];
+
+                    // Memoize the result
+                    if (dp[idx][k] != -1) {
+                        l = dp[idx][k] + 1;
+                        break;
+                    }
+
+                    dp[idx][k] = l + 1;
                     idx1 = idx;
-                    idx = mp[req];
+                    idx = k;
                     req = arr[idx1] + arr[idx];  
                     ++l;
                 }
