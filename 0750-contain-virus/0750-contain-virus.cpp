15376139c164149372f5b960a565063ac198b070
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-20 20:56:12 +0530

    Time: 15 ms (77.83%), Space: 18.8 MB (75.11%) - LeetHub

--- 0750-contain-virus/0750-contain-virus.cpp
+++ 0750-contain-virus/0750-contain-virus.cpp
@@ -44,7 +44,6 @@ class Solution {
             isInfected[i][j] = 1;
             return;
         }
-        // isInfected[i][j] = -1;
         markInfected_dfs(i - 1, j, isInfected, vis);
         markInfected_dfs(i + 1, j, isInfected, vis);
         markInfected_dfs(i, j - 1, isInfected, vis);
@@ -74,106 +73,17 @@ public:
             auto front = pq.top();
             int x = front[2];
             int y = front[3];
-            cout << "size =>" << front[1] << endl;
             ans += front[1];
             dfs2(x, y, isInfected);
-            // vis.resize(n, vector<bool>(m, false));
             vis.assign(n, vector<bool>(m, false));
             for (int i = 0; i < n; ++i) {
                 for (int j = 0; j < m; ++j) {
                     if (isInfected[i][j] == 1 && !vis[i][j]) {
-                        cout << "goes " << i << " " << j << endl;
                         markInfected_dfs(i, j, isInfected, vis);
                     }
                 }
             }
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < m; ++j) {
-                    cout << isInfected[i][j] << ' ';
-                }
-                cout << endl;
-            }
-            cout << endl;
         }
         return ans;
     }
 };
-/*
-class cluster {
-public:
-    int wc = 0;               // wall count
-    set<pair<int, int>> con;  // contaminedted (x,y) of current cluster
-    set<pair<int, int>> ucon; // un-contaminedted (x,y) of current cluster
-};
-
-class comp {
-public:
-    bool operator()(const cluster& a,const cluster& b) {
-        return a.ucon.size() < b.ucon.size();
-    }
-};
-
-class Solution {
-    int n, m;
-    void dfs(int i, int j, vector<vector<int>>& isInfected,
-             vector<vector<bool>>& vis, cluster& c) {
-        if (i < 0 || j < 0 || i >= n || j >= m || vis[i][j]==true ||
-            isInfected[i][j] == -1)
-            return;
-
-        if (isInfected[i][j] == 0) {
-            c.ucon.insert({i, j});
-            ++c.wc;
-            return;
-        }
-        vis[i][j] = true;
-        c.con.insert({i, j});
-        dfs(i - 1, j, isInfected, vis, c);
-        dfs(i + 1, j, isInfected, vis, c);
-        dfs(i, j - 1, isInfected, vis, c);
-        dfs(i, j + 1, isInfected, vis, c);
-    }
-
-
-public:
-    int containVirus(vector<vector<int>>& isInfected) {
-        n = isInfected.size();
-        m = isInfected[0].size();
-        int ans = 0;
-        while (true) {
-            vector<vector<bool>> vis(n, vector<bool>(m, false));
-            priority_queue<cluster, vector<cluster>, comp> pq;
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < m; ++j) {
-                    if (!vis[i][j] && isInfected[i][j] == 1) {
-                        cluster c;
-                        dfs(i, j, isInfected, vis, c);
-                        pq.push(c);
-                    }
-                }
-            }
-
-            if (pq.empty())
-                break;
-            cluster top = pq.top();
-            pq.pop();
-            ans += top.wc;
-            for (auto& i : top.con) {
-                int x = i.first;
-                int y = i.second;
-                isInfected[x][y] = -1;
-            }
-            while (!pq.empty()) {
-                top = pq.top();
-                pq.pop();
-                for (auto& i : top.ucon) {
-                    int x = i.first;
-                    int y = i.second;
-                    isInfected[x][y] = 1;
-                }
-            }
-        }
-        return ans;
-    }
-};
-*/
