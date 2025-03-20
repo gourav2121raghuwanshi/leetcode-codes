51049811fded055710bc67c84bc87990e2c51568
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-20 21:45:36 +0530

    Time: 488 ms (46.05%), Space: 198.5 MB (42.4%) - LeetHub

--- 0691-stickers-to-spell-word/0691-stickers-to-spell-word.cpp
+++ 0691-stickers-to-spell-word/0691-stickers-to-spell-word.cpp
@@ -1,38 +1,14 @@
 class Solution {
-    // int dp[16];
-    unordered_map<string, int> dp;
-    string getKey(vector<int>& freq) {
-        string s;
-        for (auto& i : freq) {
-            s.push_back(i + '0');
-        }
-        return s;
-    }
-    // int solve(vector<string>& stickers,vector<int>&freq){
-    //     int sum=accumulate(freq.begin(),freq.end(),0);
-    //     if(sum==0) return 0;
-    //     string key=getKey(freq);
-    //     if(dp.count(key)) return dp[key];
 
-    //     int ans=1e7;
-    //     for(auto&s:stickers){
-    //         vector<int>org=freq;
-    //         for(int j=0;j<s.size();++j){
-    //            if(freq[s[j]-'a']>0){
-    //             freq[s[j]-'a']--;
-    //             ans=min(ans,1+solve(stickers,freq));
-    //            }
-    //         }
-    //         freq=org;
-    //     }
-    //     return  dp[key]=ans;
-    //  }
+    // unordered_map<string, int> dp;
+
 public:
     int minStickers(vector<string>& stickers, string target) {
         vector<int> freq(26, 0);
         for (auto& i : target) {
             ++freq[i - 'a'];
         }
+        map<vector<int>, bool> dp;
         queue<pair<vector<int>, int>> q;
         q.push({freq, 0});
         while (!q.empty()) {
@@ -53,9 +29,8 @@ public:
                     int sum = accumulate(temp.begin(), temp.end(), 0);
                     if (sum == 0)
                         return count + 1;
-                    string key = getKey(temp);
-                    if (!dp.count(key)) {
-                        dp[key] = count + 1;
+                    if (!dp.count(temp)) {
+                        dp[temp] = 1;
                         q.push({temp, count + 1});
                     }
                 }
