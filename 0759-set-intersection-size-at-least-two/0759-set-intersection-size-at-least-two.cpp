6fdf02fe5282119a50e6994fecffd2546c72f231
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-21 12:05:57 +0530

    Time: 131 ms (8.49%), Space: 21.6 MB (98.13%) - LeetHub

--- 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.cpp
+++ 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.cpp
@@ -7,41 +7,30 @@ public:
                      return a[1] < b[1];
                  return a[0] < b[0];
              });
-        // vector<int> ans;
-        set<int> ans;
-        ans.insert(intervals[0][1] - 1);
-        ans.insert(intervals[0][1]);
+        int ans = 2;
+        int p1 = intervals[0][1] - 1;
+        int p2 = intervals[0][1] ;
         for (int i = 1; i < intervals.size(); ++i) {
-        
+            cout<<p1<<","<<p2<<endl;
             int a = intervals[i][0];
             int b = intervals[i][1];
-            int n = ans.size();
-            auto it = ans.end();
-            --it;
-            auto it1 = it;
-            --it1;
-            if (a <= *it1) {
+            if (a <= p1)
                 continue;
-            } else if (a <= *it) {
-                if (*it != b)
-                    ans.insert(b);
-                else
-                    ans.insert(b - 1);
+            else if (a <= p2) {
+                ++ans;
+                if(p2==b){
+                    p1=b-1;
+                    continue;
+                }
+                p1 = p2;
+                p2 = b;
             } else {
-                if (*it != b)
-                    ans.insert(b);
-                else
-                    ans.insert(b - 1);
-                --b;
-                auto it = ans.end();
-                --it;
-                if (*it != b)
-                    ans.insert(b);
-                else
-                    ans.insert(b - 1);
+                ans += 2;
+                p1 = b - 1;
+                p2 = b;
             }
         }
 
-        return ans.size();
+        return ans;
     }
 };
