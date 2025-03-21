a3a5ef3b2758abcc1f7045a25edaf1d9833fe4a2
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-21 11:17:14 +0530

    Time: 59 ms (38.58%), Space: 22.7 MB (13.81%) - LeetHub

---
+++ 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 if (a[1] != b[1])
+                     return a[1] < b[1];
+                 return a[0] < b[0];
+             });
+             set<int>ans;
+             ans.insert(intervals[0][1]-1);
+             ans.insert(intervals[0][1]);
+             for(int i=1;i<intervals.size();++i){
+                int a=intervals[i][0];
+                int b=intervals[i][1];
+                int c=0;
+                // cout<<"eles";
+                for(auto&i:ans){
+                    // cout<<i<<",";
+                    if(i>=a && i<=b){
+                        ++c;
+                    }
+                }
+                // cout<<c<<endl;
+                if(c>=2) continue;
+                else if(c==1){
+                    while(ans.count(b)) --b;
+                    ans.insert(b);
+                }else{
+                    while(ans.count(b)) --b;
+                    ans.insert(b);
+                    --b;
+                    while(ans.count(b)) --b;
+                    ans.insert(b);
+                    // ans.insert(b);
+                    // ans.insert(b-1);
+                }
+             }
+
+             return ans.size();
+    }
+};
