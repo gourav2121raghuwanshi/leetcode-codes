7bfe60f3a69a572cfaf1e13ea29bd491b64dd82a
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-23 15:28:12 +0530

    Time: 0 ms (100%), Space: 16.3 MB (56.88%) - LeetHub

--- 0779-max-chunks-to-make-sorted-ii/0779-max-chunks-to-make-sorted-ii.cpp
+++ 0779-max-chunks-to-make-sorted-ii/0779-max-chunks-to-make-sorted-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         // appraoch1      // logic :if at index i :
-        //  prefMax(Not Include i) <= SuffMin(include i)
+        //  prefMax(Not Include i) <= SuffMin(include i) [less equal because repeated elements]
         //   (then chunk++ at index i)
 
         // approach2 : make array b=arr
@@ -11,15 +11,20 @@ public:
         //  if at any index two sums becomes same (can make chunk at that index)
 
         int n = arr.size();
-        int ans=0;
-        vector<int>b=arr;
-        sort(b.begin(),b.end());
-        long long suma=0;
-        long long sumb=0;
-        for(int i=0;i<n;i++){
-            suma+=1LL*arr[i];
-            sumb+=1LL*b[i];
-            if(suma==sumb) ++ans;
+        int ans = 0;
+        vector<int> prefMax = arr;
+        vector<int> suffMin = arr;
+        for (int i = 1; i < n; ++i) {
+            prefMax[i] = max(prefMax[i], prefMax[i - 1]);
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            suffMin[i] = min(suffMin[i], suffMin[i + 1]);
+        }
+        for (int i = 0; i < n; ++i) {
+            int prevMax = (i > 0) ? prefMax[i - 1] : -1;
+            int nextMin = suffMin[i];
+            if (prevMax <= nextMin)
+                ++ans;
         }
         return ans;
     }
