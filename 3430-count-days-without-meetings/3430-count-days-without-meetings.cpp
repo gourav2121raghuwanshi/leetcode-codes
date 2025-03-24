628b2fd683ef3a98c3a53b98816dfb650c80d47e
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-24 12:14:27 +0530

    Time: 209 ms (10.37%), Space: 160.2 MB (7.31%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        // sort(meetings.begin(),meetings.end(),[](const vector<int>&a,const vector<int>&b){
-        //     if(a[1]!=b[1])
-        //     return a[1]>b[1];
-        //     else return a[0]<b[0];
-        // });
-        int ans=0;
-        map<int,int>mp;
-        int j=1;
-        for(auto&i:meetings){
-            mp[i[0]]+=j;
-            mp[i[1]]-=j;
-            ++j;
+        int ans = 0;
+        map<int, int> mp;
+        for (auto& i : meetings) {
+            mp[i[0]] += 1;
+            mp[i[1]] -= 1;
         }
-        int prev=1;
-        long long csum=0;
-        int c=0;
-        for(auto&i:mp){
-            cout<<i.first<<" , "<<i.second<<endl;
-            if(csum==0){
-                if(c==0) ans+=i.first-prev;
-                else if(i.first!=prev)
-                ans+=(i.first-prev-1);
+        long long csum = 0;
+        auto it = mp.begin();
+        int prev = it->first;
+        csum = it->second;
+
+        ans += prev - 1;
+        ++it;
+        while (it != mp.end()) {
+            if (csum == 0) {
+                if (it->first != prev)
+                    ans += (it->first - prev - 1);
             }
-            csum+=i.second;
-            prev=i.first;
-            ++c;
+            csum += it->second;
+            prev = it->first;
+            ++it;
         }
-        if(days!=prev)
-        ans+=days-prev;
+        
+        if (days != prev)
+            ans += days - prev;
         return ans;
     }
 };
