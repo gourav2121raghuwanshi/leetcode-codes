93cdc3bf8ffa69ccba1c56a61e5c29d86ae92a04
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-24 12:19:00 +0530

    Time: 68 ms (64.63%), Space: 142.2 MB (18.54%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
         int ans = 0;
-        map<int, int> mp;
+        vector<pair<int, int>> v;
         for (auto& i : meetings) {
-            mp[i[0]] += 1;
-            mp[i[1]] -= 1;
+            v.push_back({i[0], 1});
+            v.push_back({i[1], -1});
         }
+        sort(v.begin(), v.end());
         long long csum = 0;
-        auto it = mp.begin();
         int prev = 0;
         csum = 0;
-        while (it != mp.end()) {
+        for (auto& it : v) {
             if (csum == 0) {
-                if (it->first != prev)
-                    ans += (it->first - prev - 1);
+                if (it.first != prev)
+                    ans += (it.first - prev - 1);
             }
-            csum += it->second;
-            prev = it->first;
-            ++it;
+            csum += it.second;
+            prev = it.first;
         }
-        
         if (days != prev)
             ans += days - prev;
         return ans;
