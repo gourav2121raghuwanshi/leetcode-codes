baedd4a59f2743390416555dcd250c4bb5f5c96c
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-26 10:34:38 +0530

    Time: 43 ms (80.08%), Space: 21.8 MB (70.5%) - LeetHub

--- 0884-k-similar-strings/0884-k-similar-strings.cpp
+++ 0884-k-similar-strings/0884-k-similar-strings.cpp
@@ -1,31 +1,31 @@
 class Solution {
-    string getKey(string& a, int i) {
-        string ans = a;
-        ans += ":" + to_string(i);
-        return ans;
-    }
-    unordered_map<string, int> dp;
-    int solve(int i, string& s1, string& s2) {
-        if (i >= s1.size())
-            return 0;
-        string key = getKey(s1, i);
-        if (dp.count(key))
-            return dp[key];
-        if (s1[i] == s2[i]) {
-            return dp[key] = solve(i + 1, s1, s2);
-        } else {
-            int ans = 1e9;
-            for (int j = i + 1; j < s1.size(); ++j) {
-                if (s1[j] == s2[i]) {
-                    swap(s1[j], s1[i]);
-                    ans = min(ans, 1 + solve(i + 1, s1, s2));
-                    swap(s1[j], s1[i]);
-                }
-            }
-            return dp[key] = ans;
-        }
-    }
-
 public:
-    int kSimilarity(string s1, string s2) { return solve(0, s1, s2); }
+    int kSimilarity(string s1, string s2) {
+        if(s1.empty() || s1==s2) return 0;
+        for(int i=0;i<s1.size();++i){
+          if(s1[i]==s2[i]){
+            return kSimilarity(s1.substr(i+1),s2.substr(i+1));
+          }else{
+            vector<int>matches;
+            for(int j=i+1;j<s1.size();++j){
+              if(s1[j]==s2[i]){
+                if(s2[j]==s1[i]){
+                  swap(s1[i],s1[j]);
+                  return 1+kSimilarity(s1.substr(i+1),s2.substr(i+1));
+                }else{
+                  matches.push_back(j);
+                }
+              }
+            }
+            int ans=1e9;
+            for(auto&j:matches){
+              swap(s1[i],s1[j]);
+              ans=min(ans,1+kSimilarity(s1.substr(i+1),s2.substr(i+1)));
+              swap(s1[i],s1[j]);
+            }
+            return ans;
+          }
+        }
+        return 0;
+    }
 };
