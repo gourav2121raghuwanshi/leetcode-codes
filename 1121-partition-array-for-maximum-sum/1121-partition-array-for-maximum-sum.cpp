10872b9ac284f0ab54819c96ab544831bc43e6ac
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-26 23:26:53 +0530

    Time: 5 ms (63.27%), Space: 11.8 MB (98.58%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -1,20 +1,33 @@
 class Solution {
     int dp[501];
-    int solve(int i,vector<int>& arr, int k){
-        if(i>=arr.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int maxi=0;
-        int currmax=0;
-        for(int j=i;j<min((int)arr.size(),i+k);++j){
-            currmax=max(currmax,arr[j]);
-            maxi=max(maxi,currmax*(j-i+1)+solve(j+1,arr,k));
-        }
-        return dp[i] = maxi;
-    }
+    // int solve(int i, vector<int>& arr, int k) {
+    //     if (i >= arr.size())
+    //         return 0;
+    //     if (dp[i] != -1)
+    //         return dp[i];
+    //     int ans = 0;
+    //     int currmax = 0;
+    //     for (int j = i; j < min(i + k, (int)arr.size()); ++j) {
+    //         currmax = max(currmax, arr[j]);
+    //         ans = max(ans, currmax * (j - i + 1) + solve(j + 1, arr, k));
+    //     }
+    //     return dp[i] = ans;
+    // }
+
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        
-        memset(dp,-1,sizeof(dp));
-        return solve(0,arr,k);
+        memset(dp, 0, sizeof(dp));
+        for (int i = arr.size()-1; i >= 0; --i) {
+            int ans = 0;
+            int currmax = 0;
+            for (int j = i; j < min(i + k, (int)arr.size()); ++j) {
+                currmax = max(currmax, arr[j]);
+                ans = max(ans, currmax * (j - i + 1) + dp[j+1]);
+            }
+            dp[i] = ans;
+        }
+
+        return dp[0];
+        // return solve(0, arr, k);
     }
 };
