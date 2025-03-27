00b7ac056d6c4c082b4e689e771b28de925853f2
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-27 19:06:52 +0530

    Time: 38 ms (21.47%), Space: 17.4 MB (79.67%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,20 +1,42 @@
 class Solution {
-    int dp[1001][1001];
-    int solve(int i,int j,string &s, string &t){
-        if(j>=t.size()) return 1;
-        if(i>=s.size()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
-            int take=solve(i+1,j+1,s,t);
-            int ntake=solve(i+1,j,s,t);
-            return dp[i][j] = take+ntake;
-        }else{
-            return dp[i][j] = solve(i+1,j,s,t);
-        }
-    }
+    long long dp[1001][1001];
+    // int solve(int i, int j, string& s, string& t) {
+    //     if (j >= t.size())
+    //         return 1;
+    //     if (i >= s.size())
+    //         return 0;
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
+    //     if (s[i] == t[j]) {
+    //         int take = solve(i + 1, j + 1, s, t);
+    //         int ntake = solve(i + 1, j, s, t);
+    //         return dp[i][j] = take + ntake;
+    //     } else {
+    //         return dp[i][j] = solve(i + 1, j, s, t);
+    //     }
+    // }
+
 public:
     int numDistinct(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,s,t);
+        memset(dp, 0, sizeof(dp));
+        int n = s.size();
+        int m = t.size();
+        // Base case
+        for(int i=0;i<=n;i++){
+            dp[i][m]=1;
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (s[i] == t[j]) {
+                    long long take = dp[i+1][j+1];
+                    long long ntake =dp[i+1][j]; 
+                     dp[i][j] = (take + ntake)%((int)1e9+7);
+                } else {
+                    dp[i][j] =dp[i+1][j] ;
+                }
+            }
+        }
+        return dp[0][0];
+        // return solve(0, 0, s, t);
     }
 };
