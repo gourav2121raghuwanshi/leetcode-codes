5f67038e4a35cf454b500180e0c52555b2f78b36
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-27 18:47:59 +0530

    Time: 18 ms (94.3%), Space: 13.3 MB (92.34%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,49 +1,56 @@
 class Solution {
 public:
-
-    string shortestCommonSupersequence(string str1, string str2) {
-    int n = str1.size();
-    int m = str2.size();
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-    for (int i = n - 1; i >= 0; --i) {
-        for (int j = m - 1; j >= 0; --j) {
-            if (str1[i] == str2[j]) {
-                dp[i][j] = 1 + dp[i + 1][j + 1];
-            } else {
-                dp[i][j] = max(dp[i][j + 1], dp[i + 1][j]);
+    string shortestCommonSupersequence(string a, string b) {
+        int n=a.size();
+        int m=b.size();
+           int dp[1001][1001];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (a[i - 1] == b[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
-    }
-    int len=dp[0][0];
-    string ans="";
-   
-    int i=0;
-    int j=0;
-    while(i<n && j<m)
-    {
-        if(str1[i]==str2[j]){
-            ans+=str1[i];
-            ++i;
-            ++j;
+        string ans;
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;++j){
+        //         cout<<dp[i][j]<<' ';
+        //     }
+        //     cout<<endl;
+        // }
+        // int len=dp[n][m];
+        // // cout<<len<<endl;
+        // string ans(len,'#');
+        // int idx=len-1;
+        int i=n;
+        int j=m;
+        while(i>0 && j>0){
+            if(a[i-1]==b[j-1]){
+                ans.push_back(a[i-1]);
+                --i,--j;
+            }else{
+                if(dp[i-1][j]>dp[i][j-1]){
+                    ans.push_back(a[i-1]);
+                    --i;
+                }else if(dp[i-1][j]<=dp[i][j-1]){
+                    ans.push_back(b[j-1]);
+                    --j;
+                }
+            }
         }
-        else if(dp[i+1][j]>dp[i][j+1]){
-            ans+=str1[i];
-            ++i;
+        reverse(ans.begin(),ans.end());
+        if(i>0){
+            ans=a.substr(0,i)+ans;
         }
-        else{ans+=str2[j]; ++j;}
-    }
-
-
-    while(i<n){
-         ans+=str1[i];
-         ++i;
-    }
-    while(j<m){
-        ans+=str2[j]; 
-        ++j;
-    }
-    return ans;
-
+        if(j>0){
+            ans=b.substr(0,j)+ans;
+        }
+        
+        // cout<<ans<<endl;
+        return ans;
     }
 };
