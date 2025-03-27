f151df6fec353bea348571a282696023062726b2
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-27 21:31:31 +0530

    Time: 66 ms (85.91%), Space: 15.6 MB (78.77%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int dp[201][10001];
+    // int dp[201][10001];
     // bool solve(int i, vector<int>& nums, int sum) {
     //     if (sum == 0)
     //         return true;
@@ -13,24 +13,27 @@ class Solution {
 
 public:
     bool canPartition(vector<int>& nums) {
-        memset(dp, 0, sizeof(dp));
+        // memset(dp, 0, sizeof(dp));
         int Totalsum = accumulate(nums.begin(), nums.end(), 0);
         if (Totalsum & 1)
             return false;
+        vector<int>curr(Totalsum/2+1,0);
+        vector<int>prev(Totalsum/2+1,0);
         for (int i = 0; i < 201; i++) {
-            dp[i][0] = true;
+            prev[0] = true;
         }
         int n = nums.size();
         for (int i = n - 1; i >= 0; --i) {
             for (int sum = 0; sum <= Totalsum / 2; ++sum) {
-                dp[i][sum] =
-                    dp[i + 1][sum] ||
-                    (sum - nums[i] >= 0 ? dp[i + 1][sum - nums[i]] : false);
+                curr[sum] =
+                    prev[sum] ||
+                    (sum - nums[i] >= 0 ? prev[sum - nums[i]] : false);
                 //  solve(i + 1, nums, sum - nums[i]) || solve(i + 1, nums,sum);
             }
+            prev=curr;
         }
 
-        return dp[0][Totalsum / 2];
+        return prev[Totalsum / 2];
         // return solve(0, nums, sum / 2);
     }
 };
