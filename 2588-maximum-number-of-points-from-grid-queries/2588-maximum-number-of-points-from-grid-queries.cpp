723dbad3998464d99627d976a6c8594fdcf5cea6
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-28 22:20:05 +0530

    Time: 342 ms (18.48%), Space: 80.2 MB (19.57%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
@@ -6,41 +6,42 @@ public:
         priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
         pq.push({grid[0][0], 0, 0});
         int count = 0;
-        map<int, int> mp;
-        for(auto&i:queries){
-            mp[i]=1;
+        vector<pair<int, int>> q;
+        // map<int, int> mp;
+        for (int i = 0; i < queries.size(); ++i) {
+            q.push_back({queries[i], i});
         }
-        vector<pair<int,int>>dir={
-{0,1},{0,-1},{1,0},{-1,0}
-        };
-        vector<vector<int>>vis(n,vector<int>(m,false));
-        vis[0][0]=true;
-        for(auto&i:mp){
-            int val=i.first;
-            if(pq.empty() || pq.top()[0]>=val){
-                mp[val]=count;
+        sort(q.begin(), q.end());
+        vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        vector<vector<int>> vis(n, vector<int>(m, false));
+        vector<int> ans(queries.size());
+        vis[0][0] = true;
+        for (auto& i : q) {
+            int val = i.first;
+            int idx=i.second;
+            if (pq.empty() || pq.top()[0] >= val) {
+                ans[idx]=count;
                 continue;
             }
-            while(!pq.empty()){
-                auto top=pq.top();
-                if(top[0]>=val)break;
+            while (!pq.empty()) {
+                auto top = pq.top();
+                if (top[0] >= val)
+                    break;
                 pq.pop();
                 ++count;
-                for(auto&i:dir){
-                    int dx=i.first+top[1];
-                    int dy=i.second+top[2];
-                    if(dx>=0 && dx<n && dy>=0 && dy<m && !vis[dx][dy]){
-                        vis[dx][dy]=true;
-                        pq.push({grid[dx][dy],dx,dy});
+                for (auto& i : dir) {
+                    int dx = i.first + top[1];
+                    int dy = i.second + top[2];
+                    if (dx >= 0 && dx < n && dy >= 0 && dy < m &&
+                        !vis[dx][dy]) {
+                        vis[dx][dy] = true;
+                        pq.push({grid[dx][dy], dx, dy});
                     }
                 }
             }
-            mp[val]=count;
+            ans[idx] = count;
         }
-        vector<int>ans;
-        for(auto&i:queries){
-            ans.push_back(mp[i]);
-        }
+       
         return ans;
     }
 };
