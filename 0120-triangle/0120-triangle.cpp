f9ca900f313aa44fabd6f281fdeb485c0180a915
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-29 00:10:07 +0530

    Time: 0 ms (100%), Space: 12.8 MB (66.76%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,28 @@
 class Solution {
-    int n,m;
-    int dp[201][201];
+    int n, m;
+    // int dp[201][201];
     // int solve(int i,int j,vector<vector<int>>& triangle){
     //     if(i==n) return 0;
     //     if(j>=triangle[i].size()) return 1e7;
 
     //     if(dp[i][j]!=-1) return dp[i][j];
-    //     return dp[i][j] = triangle[i][j]+min(solve(i+1,j,triangle),solve(i+1,j+1,triangle));
+    //     return dp[i][j] =
+    //     triangle[i][j]+min(solve(i+1,j,triangle),solve(i+1,j+1,triangle));
     // }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        n=triangle.size();
-        m=triangle[0].size();
-        memset(dp,0,sizeof(dp));
-        for(int i=n-1;i>=0;--i){
-            for(int j=triangle[i].size()-1;j>=0;--j){
-             dp[i][j] = triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+        n = triangle.size();
+        m = triangle[n-1].size();
+        // memset(dp,0,sizeof(dp));
+        vector<int> curr(m + 1, 0);
+        vector<int> prev(m + 1, 0);
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = triangle[i].size() - 1; j >= 0; --j) {
+                curr[j] = triangle[i][j] + min(prev[j], prev[j + 1]);
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return curr[0];
         // return solve(0,0,triangle);
     }
 };
