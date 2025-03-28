48260743f6cbb939d9ddf73b85f00dbbbbbae818
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-28 23:32:24 +0530

    Time: 0 ms (100%), Space: 8.3 MB (79.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,31 +1,36 @@
 class Solution {
-    int dp[101][101];
-    int solve(int i, int j) {
-        if (i == 1 && j == 1)
-            return 1;
-        if (i <= 0 || j <= 0)
-            return 0;
+    // int dp[101][101];
+    // int solve(int i, int j) {
+    //     if (i == 1 && j == 1)
+    //         return 1;
+    //     if (i <= 0 || j <= 0)
+    //         return 0;
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        return dp[i][j] = solve(i - 1, j) + solve(i, j - 1);
-    }
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
+    //     return dp[i][j] = solve(i - 1, j) + solve(i, j - 1);
+    // }
 
 public:
     int uniquePaths(int m, int n) {
-        memset(dp, 0, sizeof(dp));
+        // memset(dp, 0, sizeof(dp));
         // dp[1][1]=1;
-
+        vector<int>curr(n+1,0);
+        vector<int>prev(n+1,0);
+        prev[1]=1;
+        curr[1]=1;
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 if (i == 1 && j == 1)
-                    dp[i][j] = 1;
+                   continue;
                 else
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    curr[j] = prev[j] + curr[j - 1];
             }
+            prev=curr;
         }
 
-        return dp[m][n];
+        return prev[n];
+        // return dp[m][n];
         // return solve(m,n);
     }
 };
