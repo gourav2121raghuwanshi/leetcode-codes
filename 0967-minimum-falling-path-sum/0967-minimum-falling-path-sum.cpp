ea4ec06123316ef422cad689950ee5ee0707e143
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-29 21:37:06 +0530

    Time: 0 ms (100%), Space: 14.3 MB (96.01%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -11,15 +11,15 @@ class Solution {
             return dp[i][j];
         if (i == 0) {
             for (int k = 0; k < m; ++k) {
-                ans = min(ans, matrix[i][k] + min({ solve(i + 1, k, matrix) ,
-                               solve(i + 1, k + 1, matrix) ,
-                               solve(i + 1, k - 1, matrix)}));
+                ans =
+                    min(ans, matrix[i][k] + min({solve(i + 1, k, matrix),
+                                                 solve(i + 1, k + 1, matrix),
+                                                 solve(i + 1, k - 1, matrix)}));
             }
         } else {
-            return  dp[i][j]  =  matrix[i][j]+min( {solve(i + 1, j, matrix) ,
-                               solve(i + 1, j + 1, matrix) ,
-                               solve(i + 1, j - 1, matrix)});
-            
+            return dp[i][j] = matrix[i][j] + min({solve(i + 1, j, matrix),
+                                                  solve(i + 1, j + 1, matrix),
+                                                  solve(i + 1, j - 1, matrix)});
         }
         return dp[i][j] = ans;
     }
@@ -31,6 +31,33 @@ public:
         for (int i = 0; i < 101; ++i)
             for (int j = 0; j < 101; ++j)
                 dp[i][j] = 1e8;
-        return solve(0, 0, matrix);
+
+        for (int j = 0; j < m; ++j) {
+            dp[n][j] = 0;
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = 0; j < m; ++j) {
+                // if (i == 0) {
+                //     for (int k = 0; k < m; ++k) {
+                //         dp[i][j] = min(dp[i][j],
+                //                        matrix[i][j] +
+                //                            min({dp[i + 1][j], dp[i + 1][j + 1],
+                //                                 (j - 1 >= 0 ? dp[i + 1][j - 1]
+                //                                             : (int)1e8)}));
+                //     }
+                // } else {
+                    dp[i][j] =
+                        matrix[i][j] +
+                        std::min({dp[i + 1][j], dp[i + 1][j + 1],
+                                  (j - 1 >= 0 ? dp[i + 1][j - 1] : (int)1e8)});
+                // }
+            }
+        }
+        int ans = 1e9;
+        for (int i = 0; i < m; ++i) {
+            ans = min(ans, dp[0][i]);
+        }
+        return ans;
     }
 };
