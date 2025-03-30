f89c014ef0c8327fcd666ee3d6fda0d2dd37ee17
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-30 11:57:10 +0530

    Time: 19 ms (57.33%), Space: 10 MB (94.35%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
-
+        // vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
+        vector<int>curr(amount + 1,0);
+        vector<int>prev(amount + 1,0);
         // Base case: There is 1 way to make amount 0 (by choosing no coins)
         for (int i = 0; i <= n; ++i) {
-            dp[i][0] = 1;
+            prev[0] = 1;
         }
 
         // Fill the DP table
         for (int i = n - 1; i >= 0; --i) {
             for (int amt = 0; amt <= amount; ++amt) {
-                long long take = 0, notTake = dp[i + 1][amt];  // Use long long
+                long long take = 0, notTake = prev[amt];  // Use long long
                 if (amt - coins[i] >= 0) {
-                    take = dp[i][amt - coins[i]];
+                    take = curr[amt - coins[i]];
                 }
-                dp[i][amt] = (int)(take + notTake);  // Cast back to int
+               curr[amt] = (int)(take + notTake);  // Cast back to int
             }
+            prev=curr;
         }
 
-        return dp[0][amount];
+        return prev[amount];
     }
 };
