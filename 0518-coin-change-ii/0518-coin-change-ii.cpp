6ee74d5cbeb77c9778202b294810cd4e72e484a4
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-30 11:44:15 +0530

    Time: 22 ms (48.24%), Space: 16.2 MB (75.11%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    int dp[301][5001];
+    int solve(int i,int amount, vector<int>& coins){
+        if(amount==0) return 1;
+        if(i>=coins.size() || amount<0) return 0;
+
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        return dp[i][amount]=  solve(i,amount-coins[i],coins)+solve(i+1,amount,coins);
+    }
+public:
+    int change(int amount, vector<int>& coins) {
+        memset(dp,-1,sizeof(dp));
+        return solve(0,amount,coins);
+    }
+};
