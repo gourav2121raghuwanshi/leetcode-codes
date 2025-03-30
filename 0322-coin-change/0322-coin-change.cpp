cbba0a11869815faab59c0da82c9c155a8ddf007
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-30 10:42:09 +0530

    Time: 27 ms (58.27%), Space: 13.1 MB (96.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,15 +1,42 @@
 class Solution {
     int dp[13][10001];
-    int solve(vector<int>& coins, int amount,int i){
-        if(amount==0) return 0;
-        if(i>=coins.size() || amount<0) return 1e7;
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        return dp[i][amount] = min(solve(coins,amount,i+1),1+solve(coins,amount-coins[i],i));
-    }
+    // int solve(vector<int>& coins, int amount, int i) {
+    //     if (amount == 0)
+    //         return 0;
+    //     if (i >= coins.size() || amount < 0)
+    //         return 1e7;
+    //     if (dp[i][amount] != -1)
+    //         return dp[i][amount];
+    //     return dp[i][amount] = min(solve(coins, amount, i + 1),
+    //                                1 + solve(coins, amount - coins[i], i));
+    // }
+
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        int ans= solve(coins,amount,0);
-        return ans==1e7?-1:ans;
+    int coinChange(vector<int>& coins, int amountt) {
+
+        int n = coins.size();
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= amountt; ++j) {
+                dp[i][j] = 1e7;
+            }
+        }
+
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int amount = 0; amount <= amountt; ++amount) {
+                int take = 1e7;
+                if (amount - coins[i] >= 0)
+                    take = 1 + dp[i][amount - coins[i]];
+                int ntake = dp[i + 1][amount];
+                dp[i][amount] = min(take, ntake);
+            }
+        }
+
+        return dp[0][amountt] == 1e7 ? -1 : dp[0][amountt];
+        // int ans = solve(coins, amount, 0);
+        // return ans == 1e7 ? -1 : ans;
     }
 };
