7aebba50be9c8e0d0fcc8c0bf959b073cbdb746d
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-31 10:49:36 +0530

    Time: 0 ms (100%), Space: 19.3 MB (92.15%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,52 +1,26 @@
 class Solution {
-    class DSU {
-        vector<int> rank;
-        vector<int> parent;
-
-    public:
-        DSU(int n) {
-            rank.resize(n, 0);
-            parent.resize(n);
-            iota(parent.begin(), parent.end(), 0);
-        }
-        int findParent(int a) {
-            if (a == parent[a])
-                return a;
-            return parent[a] = findParent(parent[a]);
-        }
-        void Union(int a, int b) {
-            int x = findParent(a);
-            int y = findParent(b);
-            if (x == y)
-                return;
-            if (rank[x] > rank[y]) {
-                parent[y] = x;
-            } else if (rank[y] > rank[x]) {
-                parent[x] = y;
-            } else {
-                parent[y] = x;
-                rank[x]++;
-            }
-        }
-    };
+    vector<vector<int>> adj;
+    
+    void dfs(int node,  vector<bool>& vis,vector<vector<int>>& isConnected) {
+        if (vis[node]) return;
+        vis[node] = true;
+        
+       for(int j=0;j<isConnected[node].size();++j){
+        if(j!=node && isConnected[node][j]) dfs(j,vis,isConnected);
+       }
+    }
 
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        int ans = 0;
-        DSU prov(n);
-       for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) { 
-                if (isConnected[i][j]) {
-                    prov.Union(i, j);
-                }
+     
+        vector<bool> vis(isConnected.size(), false); // Explicit `false`
+        int ans=0;
+        for (int i = 0; i < isConnected.size(); ++i) {
+            if (!vis[i]) {
+                ++ans;
+                dfs(i, vis, isConnected);
             }
         }
-
-        unordered_set<int> uniqueParents;
-        for (int i = 0; i < n; i++) {
-            uniqueParents.insert(prov.findParent(i));
-        }
-        return uniqueParents.size();
+        return ans;
     }
 };
