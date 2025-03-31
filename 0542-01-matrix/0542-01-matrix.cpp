bbb910bcbbde2592d6e8d7778cd4b9e5e0923f34
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-31 11:55:44 +0530

    Time: 14 ms (77.84%), Space: 36.9 MB (56.47%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, -1));
-        queue<pair<int, int>> q;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (mat[i][j] == 0) {
-                    q.push({i, j});
-                    ans[i][j] = 0;
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,-1));
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(mat[i][j]==0){
+                    ans[i][j]=0;
+                    q.push({i,j});
+                    vis[i][j]=true;
                 }
             }
         }
-        int cnt = 1;
-        vector<pair<int, int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                auto front = q.front();
-                q.pop();
-                for (auto& j : dir) {
-                    int nr = front.first + j.first;
-                    int nc = front.second + j.second;
-                    if (nr >= 0 && nc >= 0 && nr < n && nc < m &&
-                        ans[nr][nc] == -1) {
-                        ans[nr][nc] = cnt;
-                        q.push({nr, nc});
+        vector<pair<int,int>>dir={
+            {0,1},{0,-1},{1,0},{-1,0}
+        };
+        int c=1;
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz--){
+                auto front=q.front(); q.pop();
+                int x=front.first;
+                int y=front.second;
+                for(auto&k:dir){
+                    int dx=k.first+x;
+                    int dy=k.second+y;
+                    if(dx>=0 && dy>=0 && dx<n && dy<m && !vis[dx][dy] && mat[dx][dy]==1){
+                        vis[dx][dy]=true;
+                        ans[dx][dy]=c;
+                        q.push({dx,dy});
                     }
                 }
             }
-            ++cnt;
+            ++c;
         }
         return ans;
     }
