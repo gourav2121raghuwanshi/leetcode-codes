dea1b66db4baf49dc8089e3dce4be03d1d26996c
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-31 15:19:25 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.65%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -3,26 +3,24 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> vis(n, -1);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; ++i) {
             if (vis[i] != -1)
                 continue;
-            vis[i] = 1;
+            vis[i] = 0;
             queue<int> q;
             q.push(i);
             while (!q.empty()) {
-                auto front = q.front();
+                auto f = q.front();
                 q.pop();
-                for (auto& j : graph[front]) {
-                    // cout<<vis[j]<<" "<<vis[front]<<endl;
+                for (auto& j : graph[f]) {
                     if (vis[j] == -1) {
-                        vis[j] = 1 - vis[front];
+                        vis[j] = 1 - vis[f];
                         q.push(j);
-                    } else if (vis[j] == vis[front])
+                    } else if (vis[j] == vis[f])
                         return false;
                 }
             }
         }
-
         return true;
     }
 };
