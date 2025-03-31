12ca69e2b35127f06099e707e8269aa0990a3c69
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-03-31 08:36:49 +0530

    Time: 970 ms (5.06%), Space: 67.5 MB (44.3%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
@@ -3,11 +3,12 @@ class Solution {
     // approach : [1,3,5,2] , k=2
     // all possible partionts
 
-    // ((1),(3,5,2)) score: 1+1+3+2 
+    // ((1),(3,5,2)) score: 1+1+3+2
     // ((1,3),(5,2)) score: 1+3+5+2
     // ((1,3,5),(2)) score: 1+5+2+2
-    // observerve 1,2 are in all scores as they are 1st and last elements of array
-    // hence discard them and the final answer won't get affected as we do substraction of maximum - minimum
+    // observerve 1,2 are in all scores as they are 1st and last elements of
+    // array hence discard them and the final answer won't get affected as we do
+    // substraction of maximum - minimum
     // ((1),(3,5,2)) score: 1+3
     // ((1,3),(5,2)) score: 3+5
     // ((1,3,5),(2)) score: 5+2
@@ -18,17 +19,21 @@ class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
         int n = weights.size();
-        vector<int> PairSum(n - 1);
-        for (int i = 0; i < n - 1; ++i) {
-            PairSum[i] = weights[i] + weights[i + 1];
+        vector<int> sum;
+        for (int i = 1; i < n; ++i) {
+            sum.push_back(weights[i] + weights[i - 1]);
         }
-        sort(PairSum.begin(), PairSum.end());
-        long long prefSumofKMinus1 = 0;
-        long long SuffSumofKMinus1 = 0;
-        for (int i = 0; i < k - 1; ++i) {
-            prefSumofKMinus1 += PairSum[i];
-            SuffSumofKMinus1 += PairSum[n - i - 2];
+        sort(sum.begin(), sum.end());
+        for(auto&i:sum) cout<<i<<" ";
+        long long ans = 0;
+        // int mini = 0;
+        int i = 0;
+        int j = sum.size() - 1;
+        --k;
+        while (k--) {
+            ans +=sum[j--];
+            ans -= sum[i++];
         }
-        return SuffSumofKMinus1 - prefSumofKMinus1;
+        return ans;
     }
 };
