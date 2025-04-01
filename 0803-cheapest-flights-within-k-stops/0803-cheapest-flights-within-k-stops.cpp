de711c74415b4cccb70fa0b2c249f5fa44f101f2
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-01 18:57:20 +0530

    Time: 4 ms (47.43%), Space: 17.9 MB (63.24%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
-                          int k) {
-        unordered_map<int, vector<pair<int, int>>> adj;
-        for (auto& i : flights) {
-            adj[i[0]].push_back({i[1], i[2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto&i:flights){
+            adj[i[0]].push_back({i[1],i[2]});
         }
-        queue<pair<int, pair<int, int>>> q;
-        q.push({0, {src, 0}}); // dist, src, cost
-        int ans = 1e9;
-        bool f = false;
-        vector<int> dis(n, 1e9);
-        dis[src] = 0;
-        while (!q.empty()) {
-            auto front = q.front();
+        queue<vector<int>>q;
+        q.push({0,0,src}); // cost/stops/node
+        vector<int>dist(n,1e9);
+        dist[src]=0;
+        while(!q.empty()){
+            auto front=q.front();
             q.pop();
-            int dist = front.first;
-            int s = front.second.first;
-            int c = front.second.second;
-            if (dist > k)
-                continue;
-            for (auto& i : adj[s]) {
-                int node = i.first;
-                int wt = i.second;
-                if (dis[node] > c + wt) {
-                    if (node == dst) {
-                        f = true;
-                        ans = min(ans, wt + c);
-                    } else {
-                        q.push({dist + 1, {node, wt + c}});
-                    }
-                    dis[node] = c + wt;
+            if(front[1]>k) continue;
+            int node=front[2];
+            int dis=front[0];
+            int stops=front[1];
+            for(auto&j:adj[node]){
+                if(dist[j.first]>dis+j.second){
+                    dist[j.first]=dis+j.second;
+                    q.push({dist[j.first],stops+1,j.first});
                 }
             }
         }
-        // cout<<ans;
 
-        if (!f)
-            return -1;
-        else
-            return ans;
+        return dist[dst]==1e9?-1:dist[dst];
     }
 };
