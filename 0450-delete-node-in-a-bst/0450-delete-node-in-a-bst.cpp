c482dc151212ae67cda4f03b5b7848b2a4d1123a
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-02 20:25:59 +0530

    Time: 0 ms (100%), Space: 34.2 MB (63.37%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -6,43 +6,42 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-    TreeNode* succ(TreeNode* root) {
-        TreeNode* succ = root->right;
-        while (succ->left)
-            succ = succ->left;
-        return succ;
+    TreeNode*predecessor(TreeNode*root){
+        TreeNode*temp=root->left;
+        while(temp->right) temp=temp->right;
+        return temp;
     }
-
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (!root)
-            return nullptr;
-        if (root->val == key) {
-            if (!root->left && !root->right) {
+        if(root==nullptr) return root;
+        if(root->val<key){
+            root->right=deleteNode(root->right,key);
+        }else if(root->val>key){
+            root->left=deleteNode(root->left,key);
+        }else{
+            if(!root->left && !root->right){
                 delete root;
                 return nullptr;
-            } else if (root->left && !root->right) {
-                TreeNode* t = root->left;
-                delete root;
-                return t;
-            } else if (root->right && !root->left) {
-                TreeNode* t = root->right;
-                delete root;
-                return t;
-            } else {
-                TreeNode* su = succ(root);
-                root->val = su->val;
-                root->right = deleteNode(root->right, su->val);
+            }else if(!root->left){
+                TreeNode*curr=root;
+                TreeNode*right=root->right;
+                delete curr;
+                return right;
+            }else if(!root->right){
+                TreeNode*curr=root;
+                TreeNode*left=root->left;
+                delete curr;
+                return left;
+            }else{
+                TreeNode*pred=predecessor(root);
+                root->val=pred->val;
+                pred->val=key;
+                root->left=deleteNode(root->left,key);
             }
-        } else if (root->val > key) {
-            root->left = deleteNode(root->left, key);
-        } else if (root->val < key) {
-            root->right = deleteNode(root->right, key);
         }
         return root;
     }
