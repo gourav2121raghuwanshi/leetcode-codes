635e390fa2af719828d8a7931aadd0dabe5c15b4
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-03 22:55:52 +0530

    Time: 0 ms (100%), Space: 8.9 MB (14.95%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,22 +1,26 @@
 class Solution {
     int dp[101][101];
-    bool solve(int i,string &s,int open){
-        if(i==s.size()){
-            return open==0;
+    bool solve(int lpc, int i, string& s) {
+        if (i >= s.size()) {
+            return lpc == 0;
         }
-        if(open<0) return false;
-        if(dp[i][open]!=-1) return dp[i][open];
-        if(s[i]=='('){
-            return dp[i][open] = solve(i+1,s,open+1);
-        }else if(s[i]==')'){
-            return dp[i][open] = solve(i+1,s,open-1);
-        }else {
-            return dp[i][open] = solve(i+1,s,open+1) || solve(i+1,s,open-1) || solve(i+1,s,open);
+        if(lpc<0) return false;
+        if (dp[i][lpc] != -1)
+            return dp[i][lpc];
+        if (s[i] == '(') {
+            return dp[i][lpc] = solve(lpc + 1, i + 1, s);
+        } else if (s[i] == ')') {
+            return dp[i][lpc] = solve(lpc-1, i + 1, s);
+        } else {
+            return dp[i][lpc] = solve(lpc + 1, i + 1, s) ||
+                                     solve(lpc-1, i + 1, s) ||
+                                     solve(lpc, i + 1, s);
         }
     }
+
 public:
     bool checkValidString(string s) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,s,0);
+        memset(dp, -1, sizeof(dp));
+        return solve( 0, 0, s);
     }
 };
