90d63fdb009f02cbe251efdc54af16adca223e37
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-04 21:44:05 +0530

    Time: 8 ms (62.39%), Space: 10.9 MB (84.86%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -3,8 +3,8 @@ public:
     int numberOfSubstrings(string s) {
         int la=-1;
         int lb=-1;
-        int lc=-1;
         int ans=0;
+        int lc=-1;
         for(int i=0;i<s.size();++i){
             if(s[i]=='a') la=i;
             else if(s[i]=='b') lb=i;
