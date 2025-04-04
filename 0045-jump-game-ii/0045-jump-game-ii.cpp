2cc834df82d83d411e18438270f7c18b58b3c709
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-04 09:49:51 +0530

    Time: 0 ms (100%), Space: 20.5 MB (34.77%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,17 +3,18 @@ public:
     int jump(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return 0;
-        int ans=1;
         int reach=nums[0];
-        int i=0;
+        int jump=1;
+        int j=0;
         while(reach<n-1){
-            int curr=0;
-            for(;i<=reach;++i){
-                curr=max(curr,i+nums[i]);
+            int curr=reach;
+            for(;j<=min(reach,n-1);++j){
+                curr=max(curr,nums[j]+j);
             }
+            if(curr==reach) return -1;
             reach=curr;
-            ++ans;
+            ++jump;
         }
-        return ans;
+        return jump;
     }
 };
