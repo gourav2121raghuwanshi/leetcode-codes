641863c1bb6eb3bcaedc569be4f1f801bfd4e9d2
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-05 21:58:22 +0530

    Time: 60 ms (87.82%), Space: 172.9 MB (90.1%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,88 +1,84 @@
+class node {
+public:
+    int key;
+    int value;
+    node* next;
+    node*prev;
+    node(int k,int v) {
+        next = nullptr;
+        prev = nullptr;
+        key = k;
+        value=v;
+    }
+};
 class LRUCache {
-    class Node {
-    public:
-        int key;
-        int data;
-        Node* prev;
-        Node* next;
-        Node(int d, int dd) {
-            key = d;
-            data = dd;
-            prev = nullptr;
-            next = nullptr;
-        }
-    };
+
+    node* head ;
+    node* tail ;
     int size;
     int curr;
-    Node* head = new Node(-1,-1);
-    Node* tail = new Node(-1,-1);
-
+    unordered_map<int,node*>mp;
 public:
     LRUCache(int capacity) {
-        size = capacity;
+        head = new node(-1,-1);
+        tail = new node(-1,-1);
+        head->next=tail;
+        tail->prev=head;
         curr = 0;
-        head->next = tail;
-        tail->prev = head;
+        size = capacity;
+    }
+    void deleteNode(node*curr){
+        node*prev=curr->prev;
+        node*next=curr->next;
+        prev->next=next;
+        next->prev=prev;
+        curr->prev=nullptr;
+        curr->next=nullptr;
+    }
+    void addAtStart(node*curr){
+        curr->next=head->next;
+        head->next->prev=curr;
+        curr->prev=head;
+        head->next=curr;
     }
 
-    unordered_map<int, Node*> mp;
-
     int get(int key) {
-        if (mp.count(key)) {
-            Node* its = mp[key];
-            Node* prev = its->prev;
-            Node* next = its->next;
-            prev->next = next;
-            next->prev = prev;
-           
-            head->next->prev = its;
-            its->next = head->next;
-            its->prev = head;
-            head->next = its;
-            
-            return its->data;
-        }
-        return -1;
+        if(mp.count(key)){
+            node*temp=mp[key];
+            int ans=temp->value;
+            deleteNode(temp);
+            addAtStart(temp);
+            return ans;
+        }else return -1;
     }
 
     void put(int key, int value) {
-        if (mp.count(key)) {
-            Node* its = mp[key];
-            Node* prev = its->prev;
-            Node* next = its->next;
-            prev->next = next;
-            next->prev = prev;
-            Node* temp = new Node(key, value);
-            head->next->prev = temp;
-            temp->next = head->next;
-            temp->prev = head;
-            head->next = temp;
-            mp[key] = temp;
-        } else {
-            if (curr >= size) {
-                Node* its = tail->prev;
-                Node* prev = its->prev;
-                Node* next = its->next;
-                prev->next = next;
-                next->prev = prev;
-                mp.erase(its->key);
-                delete its;
-
-                Node* temp = new Node(key, value);
-                head->next->prev = temp;
-                temp->next = head->next;
-                temp->prev = head;
-                head->next = temp;
-                mp[key] = temp;
-            } else {
+        if(mp.count(key)){
+            node*temp=mp[key];
+            temp->value=value;
+            deleteNode(temp);
+            addAtStart(temp);
+            mp[key]=temp;
+        }else{
+            node*newNode=new node(key,value);
+            if(curr+1<=size){
+                addAtStart(newNode);
                 ++curr;
-                Node* temp = new Node(key, value);
-                head->next->prev = temp;
-                temp->next = head->next;
-                temp->prev = head;
-                head->next = temp;
-                mp[key] = temp;
+            }else{
+                node*remove=tail->prev;
+                int key=remove->key;
+                mp.erase(key);
+                deleteNode(remove);
+                addAtStart(newNode);
             }
+            mp[key]=newNode;
         }
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
