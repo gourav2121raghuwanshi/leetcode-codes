73903817ca8fc17b5a1b4a4cb26686666deb3861
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-06 22:22:30 +0530

    Time: 511 ms (10.89%), Space: 94.1 MB (14.74%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,49 +1,63 @@
-// 3+2*4 (first * has to be evaluated)
+class Solution {
+    /*
+    CurrentResult = result_so_Far - prevValue + (prevVaue * currValue);
+    ✅ Is always correct for handling * after a + or - when parsing expressions like "2+3*4" in left-to-right       single-pass evaluation (without using a full operator precedence parser or stack), as long as you're careful with signs.
+    // 3+2*4 (first * has to be evaluated)
 // logic to evaluate * :
 // Result_so_far - prevValue + currValue * prevValue
-// ex: 2+3*4
-// Result_so_far=3+2 = 6
-//  we got : 6 - 2 + 4*2
+// ex: 2+3*4 
+// Result_so_far=3+2 = 5
+// prevValue=2
+currValue=4
+//  we got : 5 - 2 + 2*4 
 // addition and substraction are simple
+    */
 
-class Solution {
-    void solve(int i, string& s, int target, long long prevValue,
-               long long resultTillNow, string currPath, vector<string>& ans) {
-        if (i >= s.size()) {
-            if (resultTillNow == target) {
-                ans.push_back(currPath);
+
+    /*
+    sending prevValue for next iteration 
+    prevValue for + : => currValue
+    prevValue for - : => -currValue
+    prevValue for * : => prevValue*currValue
+    */
+    vector<string> ans;
+    void solve(string& num, int target, int i, string path, long long prevValue,
+               long long result_so_far) {
+        if (i >= num.size()) {
+            if (target == result_so_far) {
+                ans.push_back(path);
             }
             return;
         }
-
-        for (int j = i; j < s.size(); ++j) {
-            // Avoid numbers with leading zeros
-            if (j > i && s[i] == '0')
-                break;
-            // Extract the current number
-            string currNumStr = s.substr(i, j - i + 1);
-            long long currNum = stol(currNumStr);
-            // Base case
+        long long currValue = 0;
+        for (int j = i; j < num.size(); ++j) {
+            if (j > i && num[i] == '0')     // to avoid leading zeros and to consider trailing zeroes
+                break; /* s = "105"
+                i = 1, and s[i] == '0'
+                Then s.substr(1, 1) is "0" (which is OK),
+                but s.substr(1, 2) is "05" (which is ❌ because of leading zero).
+                */
+            currValue = currValue * 10 + (num[j] - '0');
             if (i == 0) {
-                solve(j + 1, s, target, currNum, currNum, currNumStr, ans);
+                solve(num, target, j + 1, to_string(currValue), currValue,
+                      currValue);
             } else {
-                solve(j + 1, s, target, currNum, resultTillNow + currNum,
-                      currPath + "+" + currNumStr, ans);
-                solve(j + 1, s, target, -currNum, resultTillNow - currNum,
-                      currPath + "-" + currNumStr, ans);
-                solve(j + 1, s, target, prevValue * currNum,
-                      resultTillNow - prevValue + (prevValue * currNum),
-                      currPath + "*" + currNumStr, ans);
+                solve(num, target, j + 1, path + "+" + to_string(currValue),
+                      currValue, result_so_far + currValue);
+                solve(num, target, j + 1, path + "-" + to_string(currValue),
+                      -currValue, result_so_far - currValue);
+                solve(num, target, j + 1, path + "*" + to_string(currValue),
+                     prevValue* currValue,
+                      (result_so_far - prevValue) + (prevValue * currValue));
             }
         }
     }
 
 public:
-    vector<string> addOperators(string s, int t) {
-        if (s.empty())
-            return {};
-        vector<string> ans;
-        solve(0, s, t, 0, 0, "", ans);
+    vector<string> addOperators(string num, int target) {
+
+        solve(num, target, 0, "", 0, 0);
+
         return ans;
     }
 };
