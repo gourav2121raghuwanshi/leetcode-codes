6332bbb82d16db60eb0e7de6202c24767e7c27af
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-06 19:06:18 +0530

    Time: 233 ms (73.63%), Space: 10.4 MB (94.2%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,21 +1,29 @@
 class Solution {
     int n, m;
-    bool solve(int i, int j, int idx, string& word,
-               vector<vector<char>>& board) {
-        if (idx >= word.size())
+    bool solve(int i, int j, int idx, vector<vector<char>>& board,
+               string& word) {
+        if (idx == word.size())
             return true;
-        if (i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[idx] ||
-            board[i][j] == '.')
+        if (i < 0 || j < 0 || i >= n || j >= m || board[i][j] == '*' ||
+            word[idx] != board[i][j]) {
             return false;
+        }
         char ch = board[i][j];
-        board[i][j] = '.';
-        bool ans = solve(i - 1, j, idx+1, word, board) ||
-                   solve(i + 1, j, idx+1, word, board) ||
-                   solve(i, j - 1, idx+1, word, board) ||
-                   solve(i, j + 1, idx+1, word, board);
-
+        board[i][j] = '*';
+        bool ans = solve(i + 1, j, idx + 1, board, word);
+        if (ans)
+            return true;
+        ans |= solve(i - 1, j, idx + 1, board, word);
+        if (ans)
+            return true;
+        ans |= solve(i, j - 1, idx + 1, board, word);
+        if (ans)
+            return true;
+        ans |= solve(i, j + 1, idx + 1, board, word);
         board[i][j] = ch;
-        return ans;
+        if (ans)
+            return true;
+        return false;
     }
 
 public:
@@ -25,7 +33,7 @@ public:
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 if (board[i][j] == word[0]) {
-                    if (solve(i, j, 0, word, board))
+                    if (solve(i, j, 0, board, word))
                         return true;
                 }
             }
