0fcd67e4f12a6028d414c88a2333ffa872da06a6
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-06 19:01:17 +0530

    Time: 59 ms (44.21%), Space: 79.7 MB (32.23%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,33 @@
 class Solution {
-    bool isPal(string& s, int i, int j) {
-        while (i < j) {
-            if (s[i] != s[j])
-                return false;
-            ++i, --j;
+    bool isPal(string &s){
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            ++i,--j;
         }
         return true;
     }
-    vector<vector<string>> ans;
-    void solve(int i, string& s, vector<string>& v) {
-        if (i >= s.size()) {
+     vector<vector<string>>ans;
+     void solve(int i,string&s,vector<string>&v){
+        if(i>=s.size()){
             ans.push_back(v);
             return;
         }
-
-        for (int j = i; j < s.size(); j++) {
-            // temp.push_back(s[j]);
-            if (isPal(s, i, j)) {
-                v.push_back(s.substr(i, j - i + 1));
-                solve(j + 1, s, v);
+        string temp="";
+        for(int j=i;j<s.size();++j){
+            temp.push_back(s[j]);
+            if(isPal(temp)){
+                v.push_back(temp);
+                solve(j+1,s,v);
                 v.pop_back();
             }
         }
-    }
-
+     }
 public:
     vector<vector<string>> partition(string s) {
-        vector<string> v;
-        solve(0, s, v);
-        return ans;
+       vector<string>v;
+       solve(0,s,v);
+       return ans;
     }
 };
