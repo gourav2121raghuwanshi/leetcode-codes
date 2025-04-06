e2997b7e47dc7a9bb022040f030216d6d6697308
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-06 11:46:21 +0530

    Time: 205 ms (11.42%), Space: 208.4 MB (15.22%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -5,6 +5,12 @@ class LFUCache {
     int c;
     map<int, list<vector<int>>> mp; // freq, {key,val,freq}
     unordered_map<int, list<vector<int>>::iterator> address; // key, pointer
+    /*
+        What is list<vector<int>>::iterator ?
+        This is an iterator that points to a node in the list<vector<int>>.
+        It behaves like a pointer to a vector<int>.
+        meaning we just gets the reference to vector<int> of the current key
+    */
 public:
     LFUCache(int capacity) {
         size = capacity;
@@ -16,9 +22,11 @@ public:
             vector<int> v = *address[key];
             int val = v[1];
             int f = v[2];
+
             mp[f].erase(address[key]);
             if (mp[f].empty())
                 mp.erase(f);
+
             ++f;
             v[2] = f;
             mp[f].push_front(v);
@@ -47,21 +55,23 @@ public:
         } else {
             if (c + 1 <= size) {
                 ++c;
-                vector<int> v = {key, value, 1};
-                mp[1].push_front(v);
+                vector<int> newNode = {key, value, 1};
+                mp[1].push_front(newNode);
                 address[key] = mp[1].begin();
             } else {
-                auto& to_delete = mp.begin()->second;
-                int f = to_delete.back()[2];
-                int v = to_delete.back()[1];
-                int k = to_delete.back()[0];
+                list<vector<int>>& to_delete = mp.begin()->second;              // get the minFreq list
+                vector<int> last = to_delete.back(); // to be deleted
+                int f = last[2];
+                int k = last[0];
                 mp[f].erase(address[k]);
 
                 if (mp[f].empty())
                     mp.erase(f);
-                address.erase(k);
-                vector<int> vv = {key, value, 1};
-                mp[1].push_front(vv);
+
+                address.erase(k); // remove old key's address
+
+                vector<int> newNode = {key, value, 1};
+                mp[1].push_front(newNode);
                 address[key] = mp[1].begin();
             }
         }
