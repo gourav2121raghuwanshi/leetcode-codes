f10ec5628e6be5650f44c89771927d8408935f8a
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-07 15:51:07 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.78%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,27 +1,26 @@
 class Solution {
-    double solve(double x, long long n) {
-        if (n == 0) return 1;
-        if (n == 1) return x;
-
-        double half = solve(x, n / 2);
-
-        if (n % 2 == 0) return half * half;
-        else return x * half * half;
-    }
-
 public:
-    double myPow(double x, int n) {
-        if (x == 1.0) return 1.0;
-
-        long long N = n;
-        bool neg = false;
-
-        if (N < 0) {
-            neg = true;
-            N = -N;
+    double myPow(double x, int exp) {
+        if(x==1) return 1;
+        double ans=1;
+        long long n=exp;
+        bool f=false;
+        if(n<0){
+            f=true;
+            n= -n;
+            x=1/x;
         }
 
-        double ans = solve(x, N);
-        return neg ? 1.0 / ans : ans;
+        while(n>0){
+            if(n&1){
+                ans=ans*x;
+            }
+                n/=2;
+                x*=x;
+        
+        }
+
+
+       return ans;
     }
 };
