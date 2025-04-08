25099eae44c00a5108dd90e9659708038316a4ce
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-08 18:17:41 +0530

    Time: 402 ms (33.42%), Space: 127.2 MB (7.47%) - LeetHub

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/2162-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/2162-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,9 +1,9 @@
 class Solution {
-// video : https://www.youtube.com/watch?v=45TkgKy0GS0
+    // video : https://www.youtube.com/watch?v=45TkgKy0GS0
 public:
     int minimumDifference(vector<int>& nums) {
         long long ts = accumulate(nums.begin(), nums.end(), 0LL);
-        vector<int> a, b;
+
         int N = nums.size();
 
         int n = N / 2;
@@ -30,24 +30,24 @@ public:
 
         long long ans = LLONG_MAX;
 
-        ans=min(ans,abs(ts-2*left[n][0])); // all n ele from left
-        ans=min(ans,abs(ts-2*right[n][0])); // all n ele from right
+        ans = min(ans, abs(ts - 2 * left[n][0]));  // all n ele from left
+        ans = min(ans, abs(ts - 2 * right[n][0])); // all n ele from right
 
-        // left right (taking elents)
+        // left right (taking elements)
         // 1     n-1
         // 2     n-2
         // .       .
         // n-1    1
         for (int size = 1; size <= n - 1; ++size) {
             for (auto& a : left[size]) {
-                int b = (ts - 2 * a) / 2;
-                auto& v = right[n - size];
+                int b = (ts - 2 * a) / 2;  // target we are looking for so to get as close to ts/2 as possible
+                auto& v = right[n - size];   // take n-size elements from right
                 auto itr = lower_bound(v.begin(), v.end(), b);
 
                 // *itr => value at that index
                 if (itr != v.end()) {
                     ans = min(ans, abs(ts - 2 * (a + *itr)));
-                }
+                }                                   
                 if (itr != v.begin()) {
                     --itr;
                     ans = min(ans, abs(ts - 2 * (a + *itr)));
