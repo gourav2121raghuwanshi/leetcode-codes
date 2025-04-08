10976fe6d23edce38a33399de7eb396dea12868a
Gourav Raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2025-04-08 18:04:56 +0530

    Time: 387 ms (43.01%), Space: 126.9 MB (9.59%) - LeetHub

---
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/2162-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -0,0 +1,59 @@
+class Solution {
+// video : https://www.youtube.com/watch?v=45TkgKy0GS0
+public:
+    int minimumDifference(vector<int>& nums) {
+        long long ts = accumulate(nums.begin(), nums.end(), 0LL);
+        vector<int> a, b;
+        int N = nums.size();
+
+        int n = N / 2;
+        vector<vector<long long>> left(n + 1);
+        vector<vector<long long>> right(n + 1);
+
+        for (int i = 0; i < (1 << n); ++i) { // i => mask
+            long long sleft = 0;
+            long long sright = 0;
+            int size = 0;
+            for (int j = 0; j < n; ++j) {
+                if (i & (1 << j)) {
+                    sleft += nums[j];
+                    sright += nums[j + n];
+                    ++size;
+                }
+            }
+            left[size].push_back(sleft);
+            right[size].push_back(sright);
+        }
+
+        for (auto& i : right)
+            sort(i.begin(), i.end());
+
+        long long ans = LLONG_MAX;
+
+        ans=min(ans,abs(ts-2*left[n][0])); // all n ele from left
+        ans=min(ans,abs(ts-2*right[n][0])); // all n ele from right
+
+        // left right (taking elents)
+        // 1     n-1
+        // 2     n-2
+        // .       .
+        // n-1    1
+        for (int size = 1; size <= n - 1; ++size) {
+            for (auto& a : left[size]) {
+                int b = (ts - 2 * a) / 2;
+                auto& v = right[n - size];
+                auto itr = lower_bound(v.begin(), v.end(), b);
+
+                // *itr => value at that index
+                if (itr != v.end()) {
+                    ans = min(ans, abs(ts - 2 * (a + *itr)));
+                }
+                if (itr != v.begin()) {
+                    --itr;
+                    ans = min(ans, abs(ts - 2 * (a + *itr)));
+                }
+            }
+        }
+        return ans;
+    }
+};
